1545b7003e0ce4929d474f795402c849f4a6f1b5
Seth Alt <salt@flexion.us>
2017-05-11 16:35:08 -0500

    Add special use admin screens to angular

---
+++ frontend/src/app/permit-application-list/permit-application-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-permit-application-list',
+  templateUrl: './permit-application-list.component.html',
+  styleUrls: ['./permit-application-list.component.scss']
+})
+export class PermitApplicationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
