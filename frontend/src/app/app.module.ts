4fc8b316810cea723f2fb0428e59e8a2987a30ae
Seth Alt <salt@flexion.us>
2017-05-11 16:35:08 -0500

    Add special use admin screens to angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,29 +6,40 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { ApplicationNoncommercialGroupComponent } from './application-noncommercial-group/application-noncommercial-group.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PermitApplicationListComponent } from './permit-application-list/permit-application-list.component';
+import { PermitApplicationViewComponent } from './permit-application-view/permit-application-view.component';
 import { PgFlowStep1Component } from './pg-flow-step-1/pg-flow-step-1.component';
 import { PgFlowStep2Component } from './pg-flow-step-2/pg-flow-step-2.component';
 import { PgFlowRegPersonComponent } from './pg-flow-reg-person/pg-flow-reg-person.component';
 import { PgFlowRegPersonFriendFamilyComponent } from './pg-flow-reg-person-friend-family/pg-flow-reg-person-friend-family.component';
-import { ApplicationNoncommercialGroupComponent } from './application-noncommercial-group/application-noncommercial-group.component';
 
 const appRoutes: Routes = [
+  { path: 'application-noncommercial-group', component: ApplicationNoncommercialGroupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'permit-application-list', component: PermitApplicationListComponent },
+  { path: 'permit-application-view', component: PermitApplicationViewComponent },
   { path: 'pg-flow-step-1', component: PgFlowStep1Component },
   { path: 'pg-flow-step-2', component: PgFlowStep2Component },
   { path: 'pg-flow-reg-person', component: PgFlowRegPersonComponent },
   { path: 'pg-flow-reg-person-friend-family', component: PgFlowRegPersonFriendFamilyComponent },
-  { path: 'application-noncommercial-group', component: ApplicationNoncommercialGroupComponent },
-  { path: '', component: AppComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    ApplicationNoncommercialGroupComponent,
+    HomeComponent,
+    LoginComponent,
+    PermitApplicationListComponent,
+    PermitApplicationViewComponent,
     PgFlowStep1Component,
     PgFlowStep2Component,
     PgFlowRegPersonComponent,
-    PgFlowRegPersonFriendFamilyComponent,
-    ApplicationNoncommercialGroupComponent
+    PgFlowRegPersonFriendFamilyComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/permit-application-view/permit-application-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-permit-application-view',
+  templateUrl: './permit-application-view.component.html',
+  styleUrls: ['./permit-application-view.component.scss']
+})
+export class PermitApplicationViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
