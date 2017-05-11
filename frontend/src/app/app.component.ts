85891f7c17181ac62a4b3c0131d9ed00db711b16
Eric Sorenson <eric@sorenson.software>
2017-05-11 13:21:25 -0500

    Added Layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,8 +4,8 @@ import template = require('./app.component.pug');
 @Component({
   selector: 'app-root',
   template: template.toString(),
-  // styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'app works!';
 }
