584bd158fdfc4cc4121c35e0e7a44c20ae113ede
Seth Alt <salt@flexion.us>
2017-05-12 10:52:51 -0500

    Add build tasks for frontend

--- run-builds.sh
+++ run-builds.sh
@@ -1,7 +1,11 @@
 
-cd templates
-npm install
-npm run build
+cd frontend
+npm install -g yarn
+npm install -g protractor
+yarn 
+yarn run lint
+yarn run test
+yarn run e2e
 cd ../server
 npm install
 jasmine --config=jasmine.json
