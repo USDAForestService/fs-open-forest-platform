e99ec8cbbbafe262f4165bf809c4e71a2ba02493
Eric Sorenson <eric@sorenson.software>
2017-05-12 13:17:32 -0500

    Model / validation integrations

--- frontend/src/app/application-noncommercial-group/application-noncommercial-group.component.ts
+++ frontend/src/app/application-noncommercial-group/application-noncommercial-group.component.ts
@@ -8,10 +8,59 @@ import { Component, OnInit } from '@angular/core';
 export class ApplicationNoncommercialGroupComponent implements OnInit {
 
   application = {};
+  applicantType = 'individual';
 
   states = [
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'New Hampshire ',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Carolina',
+    'North Dakota',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virginia',
+    'Washington',
+    'West Virginia',
     'Wisconsin',
-    'Illinois'
+    'Wyoming'
   ];
 
   onSubmit() {
@@ -20,9 +69,6 @@ export class ApplicationNoncommercialGroupComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
-
+  ngOnInit() { }
 
 }
