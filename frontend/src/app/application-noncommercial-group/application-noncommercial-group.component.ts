72d56fcc128f72597bcd75838c1d5edb8c24fc1b
Eric Sorenson <eric@sorenson.software>
2017-05-15 12:39:24 -0500

    show error when trying g to submit with errors

--- frontend/src/app/application-noncommercial-group/application-noncommercial-group.component.ts
+++ frontend/src/app/application-noncommercial-group/application-noncommercial-group.component.ts
@@ -7,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationNoncommercialGroupComponent implements OnInit {
 
-  application = {};
+  application = {
+    startMonth: undefined,
+    startDay: undefined,
+    startYear: undefined,
+    endMonth: undefined,
+    endDay: undefined,
+    endYear: undefined,
+  };
+
   applicantType = 'individual';
   primaryPermitHolderSameAddress = true;
   secondaryPermitHolderSameAddress = true;
+  submitted = false;
 
   states = [
     'Alabama',
@@ -68,8 +77,27 @@ export class ApplicationNoncommercialGroupComponent implements OnInit {
   hours = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
   minutes = ['00', '15', '30', '45'];
 
-  onSubmit() {
-    console.log('submit');
+  startDateChangeHandler() {
+    if (
+      this.application.startMonth &&
+      this.application.startDay &&
+      this.application.startYear &&
+      !this.application.endMonth &&
+      !this.application.endDay &&
+      !this.application.endYear
+    ) {
+      this.application.endMonth = this.application.startMonth;
+      this.application.endDay = this.application.startDay;
+      this.application.endYear = this.application.startYear;
+    }
+  }
+
+  onSubmit(form) {
+    if (!form.valid) {
+      window.scroll(0, 0);
+    } else {
+      console.log('submit');
+    }
   }
 
   constructor() { }
