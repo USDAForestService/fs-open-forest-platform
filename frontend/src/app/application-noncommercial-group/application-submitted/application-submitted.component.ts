4fb64ccf1130e62d8befae0be212b903657e782e
Seth Alt <salt@flexion.us>
2017-05-22 15:35:18 -0500

    Success page

---
+++ frontend/src/app/application-noncommercial-group/application-submitted/application-submitted.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-submitted',
+  templateUrl: './application-submitted.component.html',
+  styleUrls: ['./application-submitted.component.scss']
+})
+export class ApplicationSubmittedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
