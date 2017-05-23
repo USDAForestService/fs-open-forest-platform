f0375030af1b11e70c4e389cd6b67f9b2fdbec92
Seth Alt <salt@flexion.us>
2017-05-23 09:46:28 -0500

    Update cf cli version

--- cg-deploy/deploy.sh
+++ cg-deploy/deploy.sh
@@ -2,7 +2,7 @@ set -e
 
 export PATH=$HOME:$PATH
 #travis_retry curl -L -o $HOME/cf.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.15.0"
-curl -L -o $HOME/cf.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.15.0"
+curl -L -o $HOME/cf.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.26.0"
 tar xzvf $HOME/cf.tgz -C $HOME
 cf install-plugin autopilot -f -r CF-Community
 
