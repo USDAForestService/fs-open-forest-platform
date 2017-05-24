940b4ee035ce0a01695d67591acd681ca9c0b566
Eric Sorenson <eric@sorenson.software>
2017-05-24 07:54:29 -0500

    Tabs to spaces

--- server/.sequelize.js
+++ server/.sequelize.js
@@ -4,23 +4,23 @@ const dbParams = url.parse(process.env.DATABASE_URL, true);
 const dbAuth = dbParams.auth.split(':');
 
 const dbConfig = {
-	database: dbParams.pathname.split('/')[1],
-	username: dbAuth[0],
-	password: dbAuth[1],
-	host: dbParams.hostname,
-	port: dbParams.port,
-	dialect: dbParams.protocol.split(':')[0],
-	logging: console.log,
-	seederStorage: 'sequelize'
+  database: dbParams.pathname.split('/')[1],
+  username: dbAuth[0],
+  password: dbAuth[1],
+  host: dbParams.hostname,
+  port: dbParams.port,
+  dialect: dbParams.protocol.split(':')[0],
+  logging: console.log,
+  seederStorage: 'sequelize'
 };
 
 if (dbParams.hostname !== 'localhost') {
-	dbConfig.ssl = true;
-	dbConfig.dialectOptions = {
-		ssl:{
-			require:true
-		}
-	};
+  dbConfig.ssl = true;
+  dbConfig.dialectOptions = {
+    ssl: {
+      require:true
+    }
+  };
 }
 
 console.log(dbConfig);
