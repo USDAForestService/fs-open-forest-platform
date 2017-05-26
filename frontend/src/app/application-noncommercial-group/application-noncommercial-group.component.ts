6aeacdd17de00205ec13cd6189308169200a9df9
Seth Alt <salt@flexion.us>
2017-05-26 16:54:22 -0500

    Change Zip to ZIP

--- frontend/src/app/application-noncommercial-group/application-noncommercial-group.component.ts
+++ frontend/src/app/application-noncommercial-group/application-noncommercial-group.component.ts
@@ -125,7 +125,6 @@ export class ApplicationNoncommercialGroupComponent implements OnInit {
     this.application.noncommercialFields.endMinutes = '00';
     this.application.noncommercialFields.startDateTime = '2018-01-01T01:01:01Z';
     this.application.noncommercialFields.endDateTime = '2018-01-01T01:01:01Z';
-
   }
 
 }
