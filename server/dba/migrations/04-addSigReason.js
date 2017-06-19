d745db588e71bbc7702c64ba589b196f794f873c
David Uselmann <duselmann@flexion.us>
2017-06-19 09:32:17 -0500

    wrap DDL in a transaction

--- server/dba/migrations/04-addSigReason.js
+++ server/dba/migrations/04-addSigReason.js
@@ -4,6 +4,24 @@ let Promise = require('bluebird');
 
 const TABLE_NAME = 'noncommercialApplications';
 
+let doTransaction = (queryInterface, operations) => {
+  return queryInterface.sequelize.transaction( (trx) => {
+    return Promise.each(operations, (operation) => {
+      if (operation.operation === 'add') {
+        return queryInterface.addColumn(TABLE_NAME, operation.field, {type: operation.type}, {transaction: trx});
+      } else if (operation.operation === 'remove') {
+        return queryInterface.removeColumn(TABLE_NAME, operation.field, {transaction: trx});
+      } else if (operation.operation === 'rename') {
+        return queryInterface.renameColumn(TABLE_NAME, operation.field, operation.newField, {transaction: trx});
+      } else if (operation.operation === 'change') {
+        return queryInterface.changeColumn(TABLE_NAME, operation.field, operation.options, {transaction: trx});
+      } else if (operation.operation === 'raw') {
+        return queryInterface.sequelize.query(operation.query, { type: queryInterface.sequelize.QueryTypes.RAW, transaction: trx });
+      }
+    });
+  });
+};
+
 module.exports = {
   up: function(queryInterface, Sequelize) {
 
@@ -25,16 +43,7 @@ module.exports = {
       }
     ];
 
-    return Promise.each(operations, (operation) => {
-      if (operation.operation === 'add') {
-        return queryInterface.addColumn(TABLE_NAME, operation.field, {type: operation.type});
-      } else if (operation.operation === 'remove') {
-        return queryInterface.removeColumn(TABLE_NAME, operation.field);
-      } else if (operation.operation === 'rename') {
-        return queryInterface.renameColumn(TABLE_NAME, operation.field, operation.newField);
-      }
-    });
-
+    return doTransaction(queryInterface, operations);
   },
   down: function(queryInterface, Sequelize) {
 
@@ -53,14 +62,6 @@ module.exports = {
       }
     ];
 
-    return Promise.each(operations, (operation) => {
-      if (operation.operation === 'add') {
-        return queryInterface.addColumn(TABLE_NAME, operation.field, {type: operation.type});
-      } else if (operation.operation === 'remove') {
-        return queryInterface.removeColumn(TABLE_NAME, operation.field);
-      } else if (operation.operation === 'rename') {
-        return queryInterface.renameColumn(TABLE_NAME, operation.field, operation.newField);
-      }
-    });
+    return doTransaction(queryInterface, operations);
   }
 };
