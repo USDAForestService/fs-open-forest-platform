dc8a2aa635d628e8804ae8483f99a41d54270038
Scott Fradkin <scott@fradkin.com>
2017-06-19 15:06:50 -0500

    Issue 426 - Fix issue with async calls for middle layer tests and add more coverage

--- server/middlelayer-interaction.es6
+++ server/middlelayer-interaction.es6
@@ -26,18 +26,14 @@ module.exports = (application, successCallback, failureCallback) => {
   };
 
   request.post(authOptions, (error, response, body) => {
-    console.log('begin post to middle layer');
     if (error || response.statusCode !== 200) {
-      console.log('middle layer post to auth, failed');
       failureCallback(error || response);
     } else {
       acceptanceOptions.headers['x-access-token'] = body.token;
       request.post(acceptanceOptions, (error, response, body) => {
         if (error || response.statusCode !== 200) {
-          console.log('middle layer post to suds, failed');
           failureCallback(error || response);
         } else {
-          console.log('middle layer post, success');
           successCallback(body);
         }
       });
--- server/test/middlelayer-unit.js
+++ server/test/middlelayer-unit.js
@@ -7,50 +7,87 @@ var noncommercialModelData = require('./data/noncommercial-model-test-data.es6')
 var sendAcceptedNoncommercialApplicationToMiddleLayer = require('../middlelayer-interaction.es6');
 
 var nock = require('nock');
-var sinon = require('sinon');
 
 describe('middle layer unit tests', () => {
 
-  it('should fail middle layer auth', () => {
+  it('should fail middle layer auth', (done) => {
 
-    let failingAuth = nock('http://localhost:8080')
-                      .post('/auth')
-                      .reply(500, { status: 'fail' });
+    nock('http://localhost:8080')
+      .post('/auth')
+      .reply(500, { status: 'authfail' });
 
-    var success = sinon.spy();
-    var failure = sinon.spy();
+    var success = (response) => {
+      expect(response).fail(0, 1, 'The success callback should not have been called for this test');
+      done();
+    };
+
+    var failure = (error) => {
+      expect(error).to.not.be.null;
+      expect(error.body, 'error.body should exist').to.not.be.undefined;
+      expect(error.body.status, 'error.body.status should exist').to.not.be.undefined;
+      expect(error.body.status).to.equal('authfail');
+      done();
+    };
 
     sendAcceptedNoncommercialApplicationToMiddleLayer(noncommercialModelData.noncommercialModelPerson.create(),
       success,
       failure);
 
-    expect(success.callCount).to.equal(0);
-    expect(failure.callCount).to.equal(1);
+  });
+
+  it('should pass middle layer auth, but fail mock middle layer send', (done) => {
+    nock('http://localhost:8080')
+      .post('/auth')
+      .reply(200, { token: 'auth-token' });
+
+    nock('http://localhost:8080')
+      .post('/permits/applications/special-uses/noncommercial/')
+      .reply(500, { status: 'fail-suds' });
+
+    var success = (response) => {
+      expect(response).fail(0, 1, 'The success callback should not have been called for this test');
+      done();
+    };
+
+    var failure = (error) => {
+      expect(error).to.not.be.null;
+      expect(error.body, 'error.body should exist').to.not.be.undefined;
+      expect(error.body.status, 'error.body.status should exist').to.not.be.undefined;
+      expect(error.body.status).to.equal('fail-suds');
+      done();
+    };
+
+    sendAcceptedNoncommercialApplicationToMiddleLayer(noncommercialModelData.noncommercialModelPerson.create(),
+      success,
+      failure);
 
   });
 
-  // it('should pass middle layer auth, but fail mock middle layer send', () => {
-  //
-  //   app.post('/auth', mockAuthSuccess);
-  //   app.post('/permits/applications/special-uses/noncommercial/', mockMiddleLayerFail);
-  //   let server = app.listen(8080);
-  //
-  //   let status = undefined;
-  //   let token = undefined;
-  //
-  //   sendAcceptedNoncommercialApplicationToMiddleLayer(noncommercialModelData.noncommercialModelPerson.create(),
-  //     (response) => {
-  //       expect(response).to.not.be.undefined;
-  //     },
-  //     (error) => {
-  //       status = error.status;
-  //       token = error.token;
-  //     });
-  //
-  //   server.close();
-  //
-  //   expect(status).to.equal('fail-middle-layer');
-  //   expect(token).to.equal('token');
-  // });
+  it('should pass middle layer auth, and pass middle layer send', (done) => {
+    nock('http://localhost:8080')
+      .post('/auth')
+      .reply(200, { token: 'auth-token' });
+
+    nock('http://localhost:8080')
+      .post('/permits/applications/special-uses/noncommercial/')
+      .reply(200, { status: 'success-suds' });
+
+    var success = (response) => {
+      expect(response).to.not.be.null;
+      expect(response.status, 'response.status should exist').to.not.be.undefined;
+      expect(response.status).to.equal('success-suds');
+      done();
+    };
+
+    var failure = (error) => {
+      expect(error).fail(0, 1, 'The error callback should not have been called for this test');
+      done();
+    };
+
+    sendAcceptedNoncommercialApplicationToMiddleLayer(noncommercialModelData.noncommercialModelPerson.create(),
+      success,
+      failure);
+
+  });
 
 });
