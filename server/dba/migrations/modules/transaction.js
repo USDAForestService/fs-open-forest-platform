e04957aa5c3566c3fe0ba5ddfdb9bf76bbf6a234
David Uselmann <duselmann@flexion.us>
2017-06-20 23:01:16 -0500

    switched if to switch

--- server/dba/migrations/modules/transaction.js
+++ server/dba/migrations/modules/transaction.js
@@ -2,21 +2,34 @@
 
 let Promise = require('bluebird');
 
+
 let doTransaction = (tableName, queryInterface, operations) => {
   return queryInterface.sequelize.transaction( (trx) => {
-    return Promise.each(operations, (operation) => {
-      if (operation.operation === 'add') {
-        return queryInterface.addColumn(tableName, operation.field, {type: operation.type}, {transaction: trx});
-      } else if (operation.operation === 'remove') {
-        return queryInterface.removeColumn(tableName, operation.field, {transaction: trx});
-      } else if (operation.operation === 'rename') {
-        return queryInterface.renameColumn(tableName, operation.field, operation.newField, {transaction: trx});
-      } else if (operation.operation === 'change') {
-        return queryInterface.changeColumn(tableName, operation.field, operation.options, {transaction: trx});
-      } else if (operation.operation === 'raw') {
-        return queryInterface.sequelize.query(operation.query, { type: queryInterface.sequelize.QueryTypes.RAW, transaction: trx });
+
+    return Promise.each(moreOperations, (operation) => {
+      operation.transaction = trx;
+      switch (operation.operation) {
+        case 'add': {
+          return queryInterface.addColumn(tableName, operation.field, {type: operation.type}, {transaction: trx});
+        }
+        case 'remove': {
+          return queryInterface.removeColumn(tableName, operation.field, {transaction: trx});
+        }
+        case 'rename': {
+          return queryInterface.renameColumn(tableName, operation.field, operation.newField, {transaction: trx});
+        }
+        case 'change': {
+          return queryInterface.changeColumn(tableName, operation.field, operation.options, {transaction: trx});
+        }
+        case 'raw': {
+          return queryInterface.sequelize.query(operation.query, { type: queryInterface.sequelize.QueryTypes.RAW, transaction: trx });
+        }
+        default: {
+          return "missing operation type eg: add, change, rename, ...";
+        }
       }
     });
+
   });
 }
 
