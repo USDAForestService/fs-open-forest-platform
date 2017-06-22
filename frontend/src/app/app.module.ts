b6d7cea1374c122bc9dfdbf6bc9d6b0834f17cd2
Seth Alt <salt@flexion.us>
2017-06-21 21:44:50 -0500

    Moved application pages to application module, fixed start/end dates on admin screens

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,35 +6,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ApplicationNoncommercialGroupComponent } from './applications/application-noncommercial-group/application-noncommercial-group.component';
 import { ApplicationService } from './_services/application.service';
 import { ApplicationSubmittedComponent } from './application-submitted/application-submitted.component';
 import { ApplicationsModule } from './applications/applications.module';
-import { FileUploadComponent } from './applications/fields/file-upload.component';
-import { FileUploader, FileSelectDirective } from '../../node_modules/ng2-file-upload/ng2-file-upload';
 import { HelpMePickComponent } from './help-me-pick/help-me-pick.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PermitApplicationListComponent } from './admin/permit-application-list/permit-application-list.component';
 import { PermitApplicationViewComponent } from './admin/permit-application-view/permit-application-view.component';
 import { StyleGuideComponent } from './style-guide/style-guide.component';
-import { TemporaryOutfittersComponent } from './applications/temporary-outfitters/temporary-outfitters.component';
 import { TrackScrollDirective } from './_directives/scroll.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApplicationNoncommercialGroupComponent,
     ApplicationSubmittedComponent,
-    FileSelectDirective,
-    FileUploadComponent,
     HelpMePickComponent,
     HomeComponent,
     LoginComponent,
     PermitApplicationListComponent,
     PermitApplicationViewComponent,
     StyleGuideComponent,
-    TemporaryOutfittersComponent,
     TrackScrollDirective
   ],
   imports: [
