c452defae40e2d928134dc0bce55bb66cabe55c3
Seth Alt <salt@flexion.us>
2017-07-03 15:56:37 -0500

    Create change page title directive

---
+++ frontend/src/app/_directives/title.directive.ts
@@ -0,0 +1,34 @@
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+
+import { Directive, OnInit } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+@Directive({
+  selector: '[appPageTitle]'
+})
+
+export class TitleDirective implements OnInit {
+  constructor(
+     private router: Router,
+     private activatedRoute: ActivatedRoute,
+     private titleService: Title
+   ) {}
+   ngOnInit() {
+     this.router.events
+       .filter(event => event instanceof NavigationEnd)
+       .map(() => this.activatedRoute)
+       .map(route => {
+         while (route.firstChild) {
+           route = route.firstChild;
+           return route;
+         }
+       })
+       .filter(route => route.outlet === 'primary')
+       .filter(route => route['data']['value']['title'] !== '')
+       .mergeMap(route => route.data)
+       .subscribe((event) => this.titleService.setTitle(event['title']));
+   }
+ }
