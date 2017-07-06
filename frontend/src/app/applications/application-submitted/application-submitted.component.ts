ad14e3352adb2c8db9eb6526471cadb05e1270c9
Seth Alt <salt@flexion.us>
2017-07-06 10:18:14 -0500

    Add application submitted in applications dir

---
+++ frontend/src/app/applications/application-submitted/application-submitted.component.ts
@@ -0,0 +1,35 @@
+import { Application } from '../../_models/application';
+import { ApplicationService } from '../../_services/application.service';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  providers: [ ApplicationService ],
+  selector: 'app-application-submitted',
+  templateUrl: './application-submitted.component.html'
+})
+export class ApplicationSubmittedComponent implements OnInit {
+
+  application = new Application();
+
+  constructor(
+    private applicationService: ApplicationService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      console.log(params['id']);
+      this.applicationService.getOne(params['id'])
+        .subscribe(
+          application => this.application = application,
+          (e: any) => {
+            // TODO: Get feedback from Peter on how to handle
+            console.log(e);
+          }
+        );
+      });
+  }
+
+};
