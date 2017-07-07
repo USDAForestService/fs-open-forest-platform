09ecd291a5040d56238c0f7a6233d07f01b0cd5b
Eric Sorenson <eric@sorenson.software>
2017-07-07 15:48:00 -0500

    added (currently failing) GET test

--- server/test/tempoutfitter-tests.js
+++ server/test/tempoutfitter-tests.js
@@ -3,15 +3,16 @@
 var request = require('supertest');
 var tempOutfitterTestData = require('./data/tempOutfitterTestData.es6');
 var server = require('../app.es6');
-var testPostURL = '/permits/applications/special-uses/temp-outfitters';
+var url = '/permits/applications/special-uses/temp-outfitters';
 
 // var postFileURL = '/permits/applications/special-uses/temp-outfitters/file';
 // var mockAws = require('aws-sdk-mock');
 
 describe('temp outfitter server tests', () => {
+
   it('should return a 201 response and a db generated applicationId', (done) => {
     request(server)
-      .post(testPostURL)
+      .post(url)
       .set('Accept', 'application/json')
       .send(tempOutfitterTestData.basicTempOutfitter.create())
       .expect('Content-Type', /json/)
@@ -21,7 +22,7 @@ describe('temp outfitter server tests', () => {
 
   it('should return a 400 response and a name required error', (done) => {
     request(server)
-      .post(testPostURL)
+      .post(url)
       .set('Accept', 'application/json')
       .send(tempOutfitterTestData.basicTempOutfitter.create({ 'applicantInfo.primaryFirstName': undefined }))
       .expect('Content-Type', /json/)
@@ -31,7 +32,7 @@ describe('temp outfitter server tests', () => {
 
   it('creates a temp outfitter app with undef evening phone', (done) => {
     request(server)
-      .post(testPostURL)
+      .post(url)
       .set('Accept', 'application/json')
       .send(tempOutfitterTestData.basicTempOutfitter.create({ 'applicantInfo.eveningPhone': undefined }))
       .expect('Content-Type', /json/)
@@ -41,7 +42,7 @@ describe('temp outfitter server tests', () => {
 
   it('creates a temp outfitter app with undef fax number', (done) => {
     request(server)
-      .post(testPostURL)
+      .post(url)
       .set('Accept', 'application/json')
       .send(tempOutfitterTestData.basicTempOutfitter.create({ 'applicantInfo.faxNumber': undefined }))
       .expect('Content-Type', /json/)
@@ -50,18 +51,15 @@ describe('temp outfitter server tests', () => {
   });
 
   it('creates a temp outfitter app with too long website, 500 error', (done) => {
-
     let testData = tempOutfitterTestData.basicTempOutfitter.create();
     testData.applicantInfo.website = 'http:thisisasuperduperlongurlthatissolongitwillbreakthingsandthrowanerrorhopefullyreallythisneedstobeatleast256charactersinlengthsoletsjustcopypasteanddoublethelengthhttp:thisisasuperduperlongurlthatissolongitwillbreakthingsandthrowanerrorhopefullyreallythisneedstobeatleast256charactersinlengthsoletsjustcopypasteanddoublethelength';
-
     request(server)
-      .post(testPostURL)
+      .post(url)
       .set('Accept', 'application/json')
       .send(testData)
       .expect('Content-Type', /json/)
       .expect(500, done);
   });
-
 
 // keep these as a starting point to get the s3 mocking working
   // it('should save a file to mock s3 and the db', (done) => {
@@ -100,4 +98,37 @@ describe('temp outfitter server tests', () => {
   //
   //
   // });
+});
+
+describe('Persistence tests', () => {
+
+  let intakeControlNumber = undefined;
+
+  it('should persist an application', (done) => {
+    request(server)
+      .post(url)
+      .set('Accept', 'application/json')
+      .send(tempOutfitterTestData.basicTempOutfitter.create())
+      .expect('Content-Type', /json/)
+      .expect(function(res) {
+        // record the intake control number so that we can the the application back out
+        intakeControlNumber = res.body.appControlNumber;
+      })
+      .expect(201, done);
+  });
+
+  it('should return a persisted application', (done) => {
+    let basicTempOutfitter = tempOutfitterTestData.basicTempOutfitter.create();
+    request(server)
+      .get(url + '/' + intakeControlNumber)
+      .expect(function(res) {
+        // update the object with values only present after saving to the DB
+        basicTempOutfitter.appControlNumber = res.body.appControlNumber;
+        basicTempOutfitter.applicationId = res.body.applicationId;
+        basicTempOutfitter.createdAt = res.body.createdAt;
+        basicTempOutfitter.status = 'Received';
+      })
+      .expect(200, basicTempOutfitter, done);
+  });
+
 });
