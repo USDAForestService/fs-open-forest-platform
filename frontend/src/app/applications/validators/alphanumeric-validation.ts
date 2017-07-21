b8cdd8787b9dc2f157ac11cdad4c2fdaee328ed7
Seth Alt <salt@flexion.us>
2017-07-21 11:36:05 -0500

    add error message component

--- frontend/src/app/applications/validators/alphanumeric-validation.ts
+++ frontend/src/app/applications/validators/alphanumeric-validation.ts
@@ -3,8 +3,12 @@ import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, Validators } fr
 export function alphanumericValidator(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } => {
     const val = control.value;
-    const alphanumericRegex = /\w/;
-    const valid = alphanumericRegex.test(val);
-    return valid ? null : { alphanumericRequirement: true };
+    if (val.length) {
+      const alphanumericRegex = /\w/;
+      const valid = alphanumericRegex.test(val);
+      return valid ? null : { alphanumericRequirement: true };
+    } else {
+      return null;
+    }
   };
 }
