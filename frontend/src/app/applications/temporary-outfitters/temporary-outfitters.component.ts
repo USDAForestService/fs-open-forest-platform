84c76b81a091b5cf1108226f2aac06e7448ac176
Seth Alt <salt@flexion.us>
2017-07-21 11:36:05 -0500

    add error message component

--- frontend/src/app/applications/temporary-outfitters/temporary-outfitters.component.ts
+++ frontend/src/app/applications/temporary-outfitters/temporary-outfitters.component.ts
@@ -21,6 +21,7 @@ export class TemporaryOutfittersComponent implements OnInit {
   uploadFiles = false;
   goodStandingEvidenceMessage: string;
   orgTypeFileUpload: boolean;
+  alphanumericValidationMessage = ' requires an alphanumeric character.';
 
   applicationForm: FormGroup;
 
@@ -35,7 +36,7 @@ export class TemporaryOutfittersComponent implements OnInit {
       region: ['06', [Validators.required]],
       forest: ['05', [Validators.required]],
       type: ['tempOutfitters', [Validators.required, alphanumericValidator()]],
-      signature: ['', [Validators.required, alphanumericValidator()]],
+      signature: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(3), alphanumericValidator()]],
       applicantInfo: this.formBuilder.group({
         emailAddress: ['', Validators.required],
         organizationName: ['', alphanumericValidator()],
@@ -53,6 +54,9 @@ export class TemporaryOutfittersComponent implements OnInit {
         experienceList: ['', [alphanumericValidator()]]
       })
     });
+
+    // this.applicationForm.valueChanges.subscribe(data => this.validationStatus(this.applicationForm));
+    //  this.validationStatus();
 
     this.applicationForm.get('applicantInfo.orgType').valueChanges.subscribe(type => {
       switch (type) {
@@ -87,6 +91,63 @@ export class TemporaryOutfittersComponent implements OnInit {
       }
     });
   }
+
+  // validationStatus(data?: any) {
+  //   if (!this.applicationForm) {
+  //     return;
+  //   }
+  //   const form = this.applicationForm;
+  //
+  //   for (const field in data) {
+  //     this.formErrors[field] = '';
+  //     const control = form.get(field);
+  //     this.validateControl(control, field);
+  //     if (data[field].lenth) {
+  //       for (const f in data[field]){
+  //         const c = form.get(field.)
+  //         validateControl()
+  //       }
+  //     }
+  //   }
+  // }
+
+  // validateControl(control: any, field) {
+  //   if (control && (control.touched || control.dirty) && !control.valid) {
+  //     for (const key in control.errors) {
+  //       this.formErrors[field] += this.validationMessages[key] + ' ';
+  //     }
+  //   }
+  // }
+  //
+  // validationStatus(formGroup: FormGroup) {
+  //   (<any>Object).values(formGroup.controls).forEach(control => {
+  //     if (control.status === 'INVALID') {
+  //       console.log(control);
+  //       // control.markAsTouched();
+  //       // control.updateValueAndValidity();
+  //     }
+  //     if (control.controls) {
+  //       this.validationStatus(control);
+  //     }
+  //   });
+  // }
+
+  // formErrors = {};
+  //
+  // validationMessages = {
+  //   required: ' is required.',
+  //   minlength: ' requires a minimum of 2 characters.',
+  //   maxlength: ' requires a maximum of 3 characters.',
+  //   alphanumericRequirement: this.alphanumericValidationMessage
+  // };
+  //
+  // parseErrors(errors) {
+  //   let message = '';
+  //   if (errors.required) {
+  //     message += 'Field is required';
+  //   }
+  //   return message;
+  // }
 
   onSubmit(form) {
     this.submitted = true;
