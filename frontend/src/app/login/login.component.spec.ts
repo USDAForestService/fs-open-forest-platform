9845945b15343f9a6b0a20686151929ecaa22606
Seth Alt <salt@flexion.us>
2017-07-27 15:34:21 -0500

    Add providers to unit tests

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -1,15 +1,23 @@
+import { AuthenticationService } from './../_services/authentication.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LoginComponent } from './login.component';
+import { MockActivatedRoute, MockRouter } from '../_mocks/routes.mock';
+import { Router } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let mockRouter: MockRouter;
 
   beforeEach(
     async(() => {
+      mockRouter = new MockRouter();
       TestBed.configureTestingModule({
-        declarations: [LoginComponent]
+        declarations: [LoginComponent],
+        providers: [AuthenticationService, FormBuilder, { provide: Router, useValue: mockRouter }],
+        schemas: [NO_ERRORS_SCHEMA]
       }).compileComponents();
     })
   );
