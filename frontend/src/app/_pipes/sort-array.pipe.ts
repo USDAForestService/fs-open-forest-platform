2d9c59fa682d088c13e1ec8b1171b6fc031162dc
Seth Alt <salt@flexion.us>
2017-08-11 17:00:24 -0500

    fix lint errors

--- frontend/src/app/_pipes/sort-array.pipe.ts
+++ frontend/src/app/_pipes/sort-array.pipe.ts
@@ -8,11 +8,11 @@ export class SortArray implements PipeTransform {
     if (typeof args[0] === 'undefined') {
       return array;
     }
-    let direction = args[0][0];
-    let column = args.replace('-', '');
+    const direction = args[0][0];
+    const column = args.replace('-', '');
     array.sort((a: any, b: any) => {
-      let left = Number(new Date(a[column]));
-      let right = Number(new Date(b[column]));
+      const left = Number(new Date(a[column]));
+      const right = Number(new Date(b[column]));
       return direction === '-' ? right - left : left - right;
     });
     return array;
