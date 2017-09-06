8005366b8d082643e5692757bd2f2bd585076edf
Seth Alt <salt@flexion.us>
2017-09-06 15:20:28 -0500

    remove hard-coded urls, replace with env vars

--- frontend/src/app/login/authenticated.component.ts
+++ frontend/src/app/login/authenticated.component.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '../_services/auth.guard';
 import { Component, Input } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-authenticated',
@@ -16,6 +17,10 @@ export class AuthenticatedComponent {
     private authGuard: AuthGuard,
     private router: Router
   ) {}
+
+  login() {
+    window.location.href = environment.apiUrl + 'auth/login-gov/openid/login';
+  }
 
   logout() {
     this.authenticationService.logout();
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://fs-intake-api-staging.app.cloud.gov/',
+  //apiUrl: 'https://fs-intake-api-staging.app.cloud.gov/',
+  apiUrl: 'https://fs-intake-api-login-test.app.cloud.gov/',
   buildDate: 'Mon Jun 12 2017 22:17:33 GMT-0500 (CDT)',
   version: '0.0.0'
 };
