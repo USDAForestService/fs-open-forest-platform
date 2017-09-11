626101ec130d1eee529e638805706d504b805189
Seth Alt <salt@flexion.us>
2017-09-11 15:40:46 -0500

    fix routing on faq page

--- frontend/src/app/applications/temporary-outfitters/temporary-outfitters-faq.component.ts
+++ frontend/src/app/applications/temporary-outfitters/temporary-outfitters-faq.component.ts
@@ -1,8 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { AuthenticationService } from '../../_services/authentication.service';
 
 @Component({
   selector: 'app-temporary-outfitters-faq',
   templateUrl: './temporary-outfitters-faq.component.html'
 })
-export class TemporaryOutfittersFaqComponent {}
+export class TemporaryOutfittersFaqComponent implements OnInit {
+  user: any;
+  constructor(private authentication: AuthenticationService) {}
+
+  ngOnInit() {
+    this.user = this.authentication.getUser();
+  }
+}
