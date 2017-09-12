f14405d3716327e58d5797117b92fd6717cd5193
Chris Barrett <cbarrett@flexion.us>
2017-09-12 12:35:57 -0500

    784 - making application buttons a component

--- frontend/src/app/_shared/shared.module.ts
+++ frontend/src/app/_shared/shared.module.ts
@@ -2,12 +2,12 @@ import { AppRoutingModule } from '../app-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { AppButtonComponent } from '../home/app-button.component';
 import { TrackScrollDirective } from './../_directives/scroll.directive';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [TrackScrollDirective],
-  exports: [AppRoutingModule, TrackScrollDirective, CommonModule, ReactiveFormsModule, FormsModule]
+  imports: [CommonModule,AppRoutingModule],
+  declarations: [TrackScrollDirective, AppButtonComponent],
+  exports: [AppRoutingModule, TrackScrollDirective, CommonModule, ReactiveFormsModule, FormsModule, AppButtonComponent]
 })
 export class SharedModule {}
---
+++ frontend/src/app/home/app-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { AuthenticationService } from '../_services/authentication.service';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './app-button.component.html'
+})
+
+export class AppButtonComponent {
+  @Input() link: string;
+  @Input() buttonText: string;
+
+  constructor(private authentication: AuthenticationService) {}
+}
