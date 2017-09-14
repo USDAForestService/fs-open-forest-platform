f54d49966b930d94a63deee627438b88e010ff59
Seth Alt <salt@flexion.us>
2017-09-14 15:50:52 -0500

    Stub out email to notify special use admins of new submitted application

--- frontend/src/app/login/logged-in.component.ts
+++ frontend/src/app/login/logged-in.component.ts
@@ -1,3 +1,4 @@
+import { AuthenticationService } from '../_services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
@@ -7,13 +8,17 @@ import { Router, ActivatedRoute } from '@angular/router';
   template: ''
 })
 export class LoggedInComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authentication: AuthenticationService) {}
 
   ngOnInit() {
     const requestingUrl = localStorage.getItem('requestingUrl');
+    const user = this.authentication.getUser();
+
     if (requestingUrl) {
       localStorage.removeItem('requestingUrl');
       this.router.navigate([requestingUrl]);
+    } else if (user.role === 'admin') {
+      this.router.navigate(['/admin/applications']);
     } else {
       this.router.navigate(['/']);
     }
