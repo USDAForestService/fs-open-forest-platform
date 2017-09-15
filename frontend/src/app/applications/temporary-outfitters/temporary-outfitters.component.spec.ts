11479d605c08bae6e8ee994d56b79cb1debe4df2
Chris Barrett <cbarrett@flexion.us>
2017-09-15 12:47:01 -0500

    tests for temp outfitter comp

--- frontend/src/app/applications/temporary-outfitters/temporary-outfitters.component.spec.ts
+++ frontend/src/app/applications/temporary-outfitters/temporary-outfitters.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import * as sinon  from 'sinon';
 import { TemporaryOutfittersComponent } from './temporary-outfitters.component';
+import { ApplicationService } from '../../_services/application.service';
+import { ApplicationFieldsService } from '../_services/application-fields.service';
+import { Observable } from 'rxjs/Observable';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
+
 
 describe('TemporaryOutfittersComponent', () => {
   let component: TemporaryOutfittersComponent;
@@ -9,7 +16,14 @@ describe('TemporaryOutfittersComponent', () => {
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [TemporaryOutfittersComponent]
+        declarations: [TemporaryOutfittersComponent],
+        schemas: [NO_ERRORS_SCHEMA],
+        providers: [
+          { provide: ApplicationService, useClass: MockApplicationService },
+          { provide: ApplicationFieldsService },
+          { provide: FormBuilder, useClass: FormBuilder}
+        ],
+        imports: [RouterTestingModule]
       }).compileComponents();
     })
   );
@@ -18,5 +32,65 @@ describe('TemporaryOutfittersComponent', () => {
     fixture = TestBed.createComponent(TemporaryOutfittersComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+  });
+
+  it('Should switch on org type', () => {
+    let orgTypes = {
+      'Person': {
+        pointOfView: 'I',
+        orgTypeFileUpload: false,
+        goodStandingEvidence: null
+      },
+      'Corporation': {
+        pointOfView: 'We',
+        orgTypeFileUpload: true,
+        goodStandingEvidence: [Validators.required]
+      },
+      'Limited Liability Company (LLC)': {
+        pointOfView: 'We',
+        orgTypeFileUpload: true,
+        goodStandingEvidence: [Validators.required]
+      },
+      'Limited Liability Partnership (LLP)': {
+        pointOfView: 'We',
+        orgTypeFileUpload: true,
+        goodStandingEvidence: [Validators.required]
+      },
+      'State Government': {
+        pointOfView: 'We',
+        orgTypeFileUpload: false,
+        goodStandingEvidence: null
+      },
+      'Local Govt': {
+        pointOfView: 'We',
+        orgTypeFileUpload: false,
+        goodStandingEvidence: null
+      },
+      'Nonprofit': {
+        pointOfView: 'We',
+        orgTypeFileUpload: true,
+        goodStandingEvidence: [Validators.required]
+      }
+    };
+    for(let type in orgTypes ) {
+      let orgFields = orgTypes[type];
+
+      let get = sinon.stub(component.applicationForm, 'get').returns(
+        {
+        setValidators: required => { expect(required).toEqual(orgFields.goodStandingEvidence) }
+        }
+      );
+      component.orgTypeChange(type);
+      expect(component.pointOfView).toEqual(orgFields.pointOfView);
+      expect(component.orgTypeFileUpload).toEqual(orgFields.orgTypeFileUpload);
+      get.restore();
+    }
   });
 });
+
+class MockApplicationService {
+  get(): Observable<{}> {
+    return Observable.of();
+  }
+}
