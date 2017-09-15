99a493df3f2db11ce143f8583933e53b508da74f
Eric Sorenson <eric@sorenson.software>
2017-09-15 09:42:42 -0500

    consistently strict

--- server/test/mock-aws-app.js
+++ server/test/mock-aws-app.js
@@ -1,15 +1,17 @@
+'use strict';
+
 var tempOutfitterTestData = require('./data/tempOutfitterTestData.es6');
 const AWS = require('mock-aws');
 
 AWS.mock('S3', 'putObject', function(params, cb) {
   cb(null, { ETag: '"82e8674bebaea2797c28872c9a38ad43"' });
 });
-AWS.mock('S3', 'getObject', function (params, cb) {
+AWS.mock('S3', 'getObject', function(params, cb) {
   cb(null, tempOutfitterTestData.mockS3Get);
 });
 
-after(function(){
-	AWS.restore();
+after(function() {
+  AWS.restore();
 });
 
 module.exports = require('../app.es6');
