352b57b76ef6e8c33b05f4960fdb033aa5a10224
Seth Alt <salt@flexion.us>
2017-09-18 16:24:04 -0500

    fix lint errors

--- frontend/src/app/_pipes/hours-from-or-date.pipe.spec.ts
+++ frontend/src/app/_pipes/hours-from-or-date.pipe.spec.ts
@@ -10,13 +10,13 @@ describe('HoursFromOrDate', () => {
 
   it('should not display date if time is less than threshold passed in', () => {
     const oneHourAgo = moment().subtract({ hours: 1, minutes: 50 });
-    const theshold: number = 24;
+    const theshold = 24;
     expect(pipe.transform(oneHourAgo, theshold)).toEqual('2 hours ago');
   });
 
   it('should display date if time is greator than threshold passed in', () => {
     const hoursAgo = moment().subtract({ hours: 25, minutes: 50 });
-    const theshold: number = 24;
+    const theshold = 24;
     expect(pipe.transform(hoursAgo, theshold)).toEqual(moment(hoursAgo, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYYY'));
   });
 });
