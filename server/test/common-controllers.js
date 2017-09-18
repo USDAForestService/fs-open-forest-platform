e3555cbf1b85652ee09672ad3e733ea0126450c3
Eric Sorenson <eric@sorenson.software>
2017-09-18 14:39:51 -0500

    added controllers directory, started middleware tests

---
+++ server/test/common-controllers.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const commonControllers = require('../controllers/common.es6');
+
+describe('common controller tests', () => {
+  describe('getAllOpenApplications', () => {
+    it('should get all open applications', done => {
+      let json = function() {
+        done();
+      };
+      let status = function(status) {
+        expect(status).to.equal(200);
+        return { json };
+      };
+      commonControllers.getAllOpenApplications(null, { status });
+    });
+  });
+});
