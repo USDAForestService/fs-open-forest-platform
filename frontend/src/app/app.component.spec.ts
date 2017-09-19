cdb0d523affb565c751a1469993b0dbb2a732dbb
Seth Alt <salt@flexion.us>
2017-09-19 11:01:26 -0500

    Unit tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,24 +1,38 @@
-import { TestBed, async } from '@angular/core/testing';
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthenticationService } from './_services/authentication.service';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, Response, ResponseOptions, XHRBackend } from '@angular/http';
 import { UsaBannerComponent } from './usa-banner/usa-banner.component';
 
 import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs/Observable';
+import { MockBackend } from '@angular/http/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
         imports: [RouterTestingModule, HttpModule],
         declarations: [AppComponent, UsaBannerComponent],
-        providers: [AuthenticationService],
+        providers: [
+          { provide: AuthenticationService, useClass: AuthenticationService },
+          { provide: XHRBackend, useClass: MockBackend }
+        ],
         schemas: [CUSTOM_ELEMENTS_SCHEMA]
       }).compileComponents();
     })
   );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it(
     'should create the app',
@@ -28,4 +42,69 @@ describe('AppComponent', () => {
       expect(app).toBeTruthy();
     })
   );
+
+  it('should go to hashtag', () => {
+    component.gotoHashtag('main-content', new MouseEvent('click'));
+    let de: DebugElement;
+    let compiled = fixture.debugElement.nativeElement;
+    const main = compiled.querySelector('#main-content');
+    main.dispatchEvent(new Event('focus'));
+    fixture.detectChanges();
+    const focus = compiled.querySelector(':focus');
+
+    expect(focus).toBe(main);
+  });
+
+  it(
+    'should check if user is authenticated',
+    inject([AuthenticationService, XHRBackend], (service, mockBackend) => {
+      const mockResponse = { email: 'test@test.com', role: 'admin' };
+      mockBackend.connections.subscribe(connection => {
+        connection.mockRespond(
+          new Response(
+            new ResponseOptions({
+              body: JSON.stringify(mockResponse)
+            })
+          )
+        );
+      });
+
+      service.getAuthenticatedUser().subscribe(user => {
+        expect(user.email).toBe('test@test.com');
+      });
+
+      const status = { message: 'test', heading: 'test' };
+      localStorage.setItem('status', JSON.stringify(status));
+      component.isAuthenticated();
+      expect(component.status.message).toBe('test');
+      expect(component.status.heading).toBe('test');
+      expect(localStorage.getItem('status')).toBeFalsy();
+
+      component.isAuthenticated();
+      expect(component.status.message).toBe('');
+      expect(component.status.heading).toBe('');
+    })
+  );
+
+  it(
+    'should throw error if error',
+    inject([AuthenticationService, XHRBackend], (service, mockBackend) => {
+      mockBackend.connections.subscribe(connection => {
+        connection.mockError(new Error('error'));
+      });
+
+      service.getAuthenticatedUser().subscribe(
+        success => {},
+        (e: any) => {
+          expect(e).toBe('error');
+        }
+      );
+    })
+  );
+
+  it('should update status', () => {
+    component.updateStatus({ heading: 'test', message: 'test message' });
+    expect(component.status.heading).toBe('test');
+    expect(component.status.message).toBe('test message');
+  });
 });
