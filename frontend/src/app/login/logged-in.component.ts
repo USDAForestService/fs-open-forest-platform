e36f2b9d4d2d736e4be044dc8e036f12ba56df96
Seth Alt <salt@flexion.us>
2017-10-05 10:16:04 -0500

    update headers to support jwts

--- frontend/src/app/login/logged-in.component.ts
+++ frontend/src/app/login/logged-in.component.ts
@@ -1,16 +1,24 @@
 import { AuthenticationService } from '../_services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-logged-in',
   template: ''
 })
 export class LoggedInComponent implements OnInit {
-  constructor(private router: Router, private authentication: AuthenticationService) {}
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private authentication: AuthenticationService
+  ) {}
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params: Params) => {
+      localStorage.setItem('token', params['token']);
+    });
+
     this.authentication.getAuthenticatedUser().subscribe((user: any) => {
       const requestingUrl = localStorage.getItem('requestingUrl');
       if (requestingUrl) {
