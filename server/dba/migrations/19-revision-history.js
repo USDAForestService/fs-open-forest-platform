6d700f9985171c202e9edb24625750107c412804
Eric Sorenson <eric@sorenson.software>
2017-10-09 13:32:47 -0500

    updatedAt field

--- server/dba/migrations/19-revision-history.js
+++ server/dba/migrations/19-revision-history.js
@@ -14,7 +14,8 @@ module.exports = {
       applicationType: { type: Sequelize.STRING, field: 'application_type', allowNull: false },
       status: { type: Sequelize.STRING, field: 'status', allowNull: false },
       email: { type: Sequelize.STRING, field: 'email', allowNull: false },
-      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW, allowNull: false, field: 'created' }
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW, allowNull: false, field: 'created' },
+      updatedAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW, allowNull: false, field: 'updated' }
     });
   },
   down: function(queryInterface) {
