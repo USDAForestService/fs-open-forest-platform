b18e28a171c2b84c91c3b7b51c11f08c2d51ff8f
Seth Alt <salt@flexion.us>
2017-10-12 15:48:47 -0500

    add sidebar component

--- frontend/src/app/_services/util.service.ts
+++ frontend/src/app/_services/util.service.ts
@@ -5,4 +5,14 @@ export class UtilService {
   convertCamelToHyphenCase(string) {
     return string.replace(/\s+/g, '-').replace(/([a-z])([A-Z])/g, '$1-$2').replace(/s+$/, '').toLowerCase();
   }
+
+  gotoHashtag(fragment: string, event, currentSection) {
+    event.preventDefault();
+    const element = document.querySelector('#' + fragment);
+    if (element) {
+      element.scrollIntoView();
+      currentSection = fragment;
+      document.getElementById(fragment).focus();
+    }
+  }
 }
