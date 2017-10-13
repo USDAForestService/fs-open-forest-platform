e55fd10084f719e0e256f90844e05f64bad1ed8c
Seth Alt <salt@flexion.us>
2017-10-13 09:55:05 -0500

    Add noncommercial learn more copy

---
+++ frontend/src/app/application-forms/temporary-outfitters/temporary-outfitters-learn-more.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-temporary-outfitters-learn-more',
+  templateUrl: './temporary-outfitters-learn-more.component.html'
+})
+export class TemporaryOutfittersLearnMoreComponent {
+  constructor() {}
+}
---
+++ frontend/src/app/sidebar/section-heading.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Input, Output, Renderer2 } from '@angular/core';
+import { UtilService } from '../_services/util.service';
+
+@Component({
+  selector: 'app-section-heading',
+  templateUrl: './section-heading.component.html'
+})
+export class SectionHeadingComponent {
+  @Input() heading: string;
+  @Input() elementId: string;
+
+  constructor(private renderer: Renderer2, private util: UtilService) {}
+
+  elementInView(event) {
+    if (event.value) {
+      this.renderer.addClass(event.target, 'in-view');
+    } else {
+      this.renderer.removeClass(event.target, 'in-view');
+    }
+
+    const viewableElements = document.getElementsByClassName('in-view');
+    if (viewableElements[0]) {
+      this.util.setCurrentSection(viewableElements[0].id);
+    }
+  }
+}
