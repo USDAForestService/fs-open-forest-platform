516925c15f9a84951696068f9ef8f1911aae1f1f
Shekar Pendem <lpendem@nciinc.com>
2017-10-16 15:58:55 -0400

    join table migration for forest and species tables

--- server/dba/migrations/21-treeSpecies.js
+++ server/dba/migrations/21-treeSpecies.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: function(queryInterface, Sequelize) {
     return queryInterface.createTable('treeSpecies', {
-      id: { type: Sequelize.INTEGER, primaryKey: true, field: 'id' },
+      id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, field: 'id' },
       name: { type: Sequelize.STRING(50), field: 'name' },
       description: { type: Sequelize.STRING, field: 'description' },
       photos: { type: Sequelize.STRING, field: 'photos' },
