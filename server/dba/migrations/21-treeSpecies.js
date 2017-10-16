7f6711efe44ad92de99c30c81283f1fe401b6efb
Shekar Pendem <lpendem@nciinc.com>
2017-10-16 15:17:33 -0400

    migrations added for forests and tree-species tables

---
+++ server/dba/migrations/21-treeSpecies.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('treeSpecies', {
+      id: { type: Sequelize.INTEGER, primaryKey: true, field: 'id' },
+      name: { type: Sequelize.STRING(50), field: 'name' },
+      description: { type: Sequelize.STRING, field: 'description' },
+      photos: { type: Sequelize.STRING, field: 'photos' },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW, allowNull: false, field: 'created' },
+      updatedAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW, allowNull: false, field: 'updated' }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('treeSpecies');
+  }
+};
