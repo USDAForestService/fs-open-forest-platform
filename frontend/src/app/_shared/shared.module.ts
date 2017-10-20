d0d2409ee5b185a31957e22788cfcd8f1a0cc5b6
Seth Alt <salt@flexion.us>
2017-10-20 12:26:50 -0500

    #6 Add allowed district information

--- frontend/src/app/_shared/shared.module.ts
+++ frontend/src/app/_shared/shared.module.ts
@@ -1,17 +1,18 @@
 import { AppRoutingModule } from '../app-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FilterPipe } from '../_pipes/filter.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppButtonComponent } from '../home/app-button.component';
 import { ProgressComponent } from '../progress/progress.component';
 import { TrackScrollDirective } from './../_directives/scroll.directive';
 import { SpacesToDashesPipe } from '../_pipes/spaces-to-dashes.pipe';
 
-
 @NgModule({
   imports: [CommonModule, AppRoutingModule],
-  declarations: [ SpacesToDashesPipe, TrackScrollDirective, AppButtonComponent, ProgressComponent],
+  declarations: [FilterPipe, SpacesToDashesPipe, TrackScrollDirective, AppButtonComponent, ProgressComponent],
   exports: [
+    FilterPipe,
     AppRoutingModule,
     TrackScrollDirective,
     CommonModule,
