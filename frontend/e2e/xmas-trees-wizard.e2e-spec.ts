b118ab86860555674a4857cde38075eb385d051b
Seth Alt <salt@flexion.us>
2017-10-24 13:29:07 -0500

    e2e tests

---
+++ frontend/e2e/xmas-trees-wizard.e2e-spec.ts
@@ -0,0 +1,74 @@
+import { TreesWizardPage } from './app.po';
+import { browser, element, by, Key } from 'protractor';
+
+describe('Xmas trees wizard page', () => {
+  let page: TreesWizardPage;
+
+  beforeEach(() => {
+    page = new TreesWizardPage();
+    browser.driver.manage().window().setSize(1400, 900);
+    page.navigateTo();
+  });
+
+  it('should have a title with forest name', () => {
+    expect<any>(element(by.css('app-root h1')).getText()).toEqual(
+      'Mt. Hood National Forest Christmas tree permit\nNov 1, 2017 - Dec 24, 2017'
+    );
+
+    expect<any>(element(by.css('.wizard-step-header')).isPresent()).toBeTruthy();
+    expect<any>(element(by.css('.wizard-step-content')).isPresent()).toBeTruthy();
+    expect<any>(element(by.css('.wizard-step-footer')).isPresent()).toBeTruthy();
+  });
+
+  it('should start on step one, and increment when you click next and prev', () => {
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('1');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('2');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('1');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('2');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('2');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('2');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('3');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('3');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('4');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('4');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('4');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('5');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('6');
+    element(by.id('next')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('7');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('6');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('5');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('4');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('4');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('4');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('3');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('3');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('2');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('2');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('2');
+    element(by.id('previous')).click();
+    expect<any>(element(by.css('.step .usa-button-inverse')).getText()).toEqual('1');
+  });
+});
