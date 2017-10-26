cb0d03eb1c7472fe9a1add20cbdaa4d15c2da8d5
Seth Alt <salt@flexion.us>
2017-10-26 12:10:07 -0500

    remove focus on click e2e tests

--- frontend/e2e/xmas-tree-sidebar.e2e-spec.ts
+++ frontend/e2e/xmas-tree-sidebar.e2e-spec.ts
@@ -1,16 +1,6 @@
 import { TreesSidebarPage } from './app.po';
 import { browser, element, by, Key, protractor } from 'protractor';
 
-const testSidebarLink = function(section) {
-  const ec = protractor.ExpectedConditions;
-  browser.wait(ec.presenceOf(element(by.id(`${section}-link`))));
-  browser.wait(ec.presenceOf(element(by.id(`${section}`))));
-  element(by.id(`${section}-link`)).click();
-  expect(element(by.id(`${section}`)).getAttribute('innerHTML')).toEqual(
-    browser.driver.switchTo().activeElement().getAttribute('innerHTML')
-  );
-};
-
 describe('Xmas sidebar wizard page', () => {
   let page: TreesSidebarPage;
 
@@ -24,22 +14,5 @@ describe('Xmas sidebar wizard page', () => {
     expect<any>(element(by.css('app-root h1')).getText()).toEqual(
       'Mt. Hood National Forest Christmas tree permit\nNov 1, 2017 - Dec 24, 2017'
     );
-  });
-
-  it(`should move focus to section on page when sidebar item is clicked`, () => {
-    testSidebarLink('general-guidelines');
-    testSidebarLink('tree-locations');
-    testSidebarLink('tree-locations-maps');
-    testSidebarLink('tree-locations-prohibited');
-    testSidebarLink('tree-locations-allowed');
-    testSidebarLink('tree-selection');
-    testSidebarLink('tree-selection-types-of-trees');
-    testSidebarLink('cutting-instructions');
-    testSidebarLink('cutting-instructions-before-you-cut');
-    testSidebarLink('cutting-instructions-when-you-cut');
-    testSidebarLink('cutting-instructions-after-you-cut');
-    testSidebarLink('trip-planning');
-    testSidebarLink('safety-first');
-    testSidebarLink('contact-information');
   });
 });
