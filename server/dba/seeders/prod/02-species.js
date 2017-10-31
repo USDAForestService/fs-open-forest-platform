8702ac6030144f612880c78b666a188bbedd5a97
cwolf1038 <cwolf123456@yahoo.com>
2017-10-31 11:56:53 -0400

    Updated drop commands

--- server/dba/seeders/prod/02-species.js
+++ server/dba/seeders/prod/02-species.js
@@ -162,6 +162,6 @@ module.exports = {
   },
   down: function(queryInterface, Sequelize) {
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete('species', { id: { [Op.in]:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16] } });
+    return queryInterface.bulkDelete('species', { id: { [Op.in]:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22] } });
   }
 };
