25d7f4459ef27b644ae679d6bb00f60c3c3b3fcf
Will Sadler <wsadler@flexion.us>
2017-10-31 12:14:55 -0500

    #36 Shoshone locations * add e2e spec (partial) * hide tree height image if no tree height in data * new blue spruce image * fix seeder for cutting height

---
+++ frontend/e2e/xmas-tree-species.e2e-spec.ts
@@ -0,0 +1,37 @@
+import { TreesSidebarPage } from './app.po';
+import { browser, element, by, Key, protractor } from 'protractor';
+
+describe('Xmas tree species page', () => {
+  let page: TreesSidebarPage;
+
+  beforeEach(() => {
+    page = new TreesSidebarPage();
+    browser.driver.manage().window().setSize(1400, 900);
+    page.navigateTo(3);
+    browser.sleep(400);
+
+  });
+
+  it('should have a species section link', () => {
+    expect<any>(element(by.id('tree-selection-link')).getText()).toEqual(
+      'Tree selection'
+    );
+  });
+
+  it('should have a tree selection header', () => {
+    expect<any>(element(by.id('tree-selection-types-of-trees')).getText()).toEqual(
+      'Types of trees'
+    );
+  });
+
+  describe('recommended species', () => {
+    let treeOne;
+    beforeEach(() => {
+      treeOne = element.all(by.repeater('tree of forest.species')).get(1);
+    });
+    it('should display Pacific Silver fir', () => {
+      expect<any>(element(by.id('tree-recommended-species')).isDisplayed()).toBeTruthy();
+    });
+  })
+
+});
