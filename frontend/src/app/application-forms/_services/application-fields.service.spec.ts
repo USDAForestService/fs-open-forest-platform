ef6ac83b94eb64fc650ad18afe6c78c5f3861be2
Seth Alt <salt@flexion.us>
2017-11-01 06:56:30 -0500

    fix unit tests

--- frontend/src/app/application-forms/_services/application-fields.service.spec.ts
+++ frontend/src/app/application-forms/_services/application-fields.service.spec.ts
@@ -118,7 +118,6 @@ describe('ApplicationFieldsService', () => {
     service.setNumberOfFiles(4);
     service.removeOneFile();
     expect(service.getNumberOfFiles()).toEqual(3);
-    expect(service.getFileUploadProgress(5)).toEqual(3);
   });
 
   it('should set file upload error', () => {
