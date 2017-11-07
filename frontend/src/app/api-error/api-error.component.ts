70987d27ff2dce46b3dc8e8afdceb1d5659bc61c
Seth Alt <salt@flexion.us>
2017-11-07 16:42:57 -0600

    add api error module

---
+++ frontend/src/app/api-error/api-error.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-api-error',
+  templateUrl: './api-error.component.html'
+})
+export class ApiErrorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
