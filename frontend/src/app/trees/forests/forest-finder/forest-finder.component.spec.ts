a742e44f4c9cd11612093b547df225fe3881ac17
cwolf1038 <cwolf123456@yahoo.com>
2017-11-07 10:50:02 -0500

    Revert "Added test for getting forest object"
    This reverts commit bb0f638fa728d22dee9def0a893c563884c82fee.

--- frontend/src/app/trees/forests/forest-finder/forest-finder.component.spec.ts
+++ frontend/src/app/trees/forests/forest-finder/forest-finder.component.spec.ts
@@ -1,12 +1,6 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
-import { HttpModule, Http, Response, ResponseOptions, XHRBackend } from '@angular/http';
-
-import { MockBackend } from '@angular/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ForestFinderComponent } from './forest-finder.component';
-import { ForestService } from '../../_services/forest.service';
 
 describe('ForestFinderComponent', () => {
   let component: ForestFinderComponent;
@@ -14,10 +8,7 @@ describe('ForestFinderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ForestFinderComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
-      providers: [{ provide: ForestService, useClass: ForestService }, { provide: XHRBackend, useClass: MockBackend }],
-      imports: [HttpModule, RouterTestingModule]
+      declarations: [ ForestFinderComponent ]
     })
     .compileComponents();
   }));
@@ -31,31 +22,4 @@ describe('ForestFinderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it(
-    'should get forest object',
-    inject([ForestService, XHRBackend], (service, mockBackend) => {
-      const mockResponse = [
-        {
-          'id': 1,
-          'forestName': 'Forest National Forest'
-        }
-      ];
-      mockBackend.connections.subscribe(connection => {
-        connection.mockRespond(
-          new Response(
-            new ResponseOptions({
-              body: JSON.stringify(mockResponse)
-            })
-          )
-        );
-      });
-
-      service.getAll().subscribe(result => {
-        expect(result[0].forestName).toBe('Forest National Forest');
-      });
-    })
-  );
-
-
 });
