974d03461c0833d3f90f74f9d061b4166a97a842
Will Sadler <wsadler@flexion.us>
2017-11-14 17:04:12 -0600

    #62 fix lint

--- frontend/src/app/trees/forests/tree-guidelines/contact-info/contact-info.component.ts
+++ frontend/src/app/trees/forests/tree-guidelines/contact-info/contact-info.component.ts
@@ -5,8 +5,11 @@ import {FilterPipe} from '../../../../_pipes/filter.pipe';
 @Component({
   selector: 'app-tree-contact-info',
   templateUrl: './contact-info.component.html',
-  providers: [FilterPipe, LineBreakFormatterPipe]
+  providers: [LineBreakFormatterPipe, FilterPipe]
 })
+
 export class ContactInfoComponent {
   @Input() forest: any;
+
+  constructor(private filter: FilterPipe, private lineBreakFormatter: LineBreakFormatterPipe) {}
 }
