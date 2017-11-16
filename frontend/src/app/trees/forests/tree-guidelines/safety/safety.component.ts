63b78746edd46f7b881449a55add5f5cbd109fd1
Seth Alt <salt@flexion.us>
2017-11-16 13:30:04 -0600

    e2e tests for planning trip. Remove safety component

--- frontend/src/app/trees/forests/tree-guidelines/safety/safety.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-tree-safety-info',
-  templateUrl: './safety.component.html'
-})
-export class TreeSafetyComponent {
-  @Input() forest: any;
-}
