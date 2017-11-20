d383344941df729320cd327f92cb92ebdcf76376
Shekar Pendem <lpendem@nciinc.com>
2017-11-20 18:10:46 -0500

    update to change fdeccribe to describe

--- frontend/e2e/xmas-tree-species.e2e-spec.ts
+++ frontend/e2e/xmas-tree-species.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { TreesSidebarPage } from './app.po';
 import { browser, element, by, Key, protractor } from 'protractor';
 
-fdescribe('Xmas tree species page', () => {
+describe('Xmas tree species page', () => {
   let page: TreesSidebarPage;
 
   describe( 'Mt Hood', () => {
