982d619066fd49e0e63b64750b1c593bd0d25210
Seth Alt <salt@flexion.us>
2017-11-27 12:47:31 -0600

    rename numOfTrees to quantity

---
+++ frontend/src/app/application-forms/fields/quantity.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { ApplicationFieldsService } from '../_services/application-fields.service';
+
+@Component({
+  selector: 'app-quantity',
+  templateUrl: './quantity.component.html'
+})
+export class QuantityComponent {
+  @Input() parentGroup: FormGroup;
+  @Input() label: string;
+  constructor(public afs: ApplicationFieldsService) {}
+}
