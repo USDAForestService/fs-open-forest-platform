832c718eb822b926643e264663e1944170b40b0b
cwolf1038 <cwolf123456@yahoo.com>
2017-11-28 15:50:06 -0500

    13 Updated HTML form and component * Added form to component * Set value of form based on params passed from frontend * Updated HTML form to be connected to component

--- frontend/src/app/pay-gov-mocks/landing-page/landing-page.component.ts
+++ frontend/src/app/pay-gov-mocks/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  applicationForm: FormGroup;
+
+  constructor(public formBuilder: FormBuilder, private activatedRoute: ActivatedRoute) {
+
+    this.applicationForm = this.formBuilder.group({
+      paymentAmount: ['$5.00', [Validators.required]],
+      cardholderName: ['', [Validators.required]],
+      cardholderBillingAddress  : ['', [Validators.required]],
+      cardholderBillingAddress2  : ['', [Validators.required]],
+      cardholderCity  : ['', [Validators.required]],
+      cardholderCountry  : ['', [Validators.required]],
+      cardholderState  : ['', [Validators.required]],
+      cardholderZip  : ['', [Validators.required]],
+      cardNumber  : ['', [Validators.required]],
+      cardExpirationMonth  : ['', [Validators.required]],
+      cardExpirationYear  : ['', [Validators.required]],
+      cardSecurityCode  : ['', [Validators.required]],
+      formName  : ['Chistmas Tree Permit - Shoshone National Forest', [Validators.required]],
+      applicantName  : ['John Doe', [Validators.required]],
+      applicantEmailAddress  : ['a@a.co', [Validators.required]],
+      selectedOption  : ['Christmas Tree Permit', [Validators.required]],
+      description  : ['Something', [Validators.required]],
+      amountOwed  : ['$5.00', [Validators.required]]
+    });
+
+  }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.applicationForm.get('paymentAmount').setValue(params['']);
+      this.applicationForm.get('formName').setValue(params['']);
+      this.applicationForm.get('applicantName').setValue(params['']);
+      this.applicationForm.get('applicantEmailAddress').setValue(params['']);
+      this.applicationForm.get('selectedOption').setValue(params['']);
+      this.applicationForm.get('description').setValue(params['']);
+      this.applicationForm.get('amountOwed').setValue(params['']);
+    });
+    this.applicationForm.get('paymentAmount').disable();
   }
 
 }
--- frontend/src/app/pay-gov-mocks/pay-gov-mocks.module.ts
+++ frontend/src/app/pay-gov-mocks/pay-gov-mocks.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   declarations: [LandingPageComponent]
 })
