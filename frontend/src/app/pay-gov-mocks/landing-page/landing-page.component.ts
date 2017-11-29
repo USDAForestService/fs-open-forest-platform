c19fc5bab18d4692008da7d62dae96b775383006
Seth Alt <salt@flexion.us>
2017-11-29 15:27:14 -0600

    Add queryParams to form

--- frontend/src/app/pay-gov-mocks/landing-page/landing-page.component.ts
+++ frontend/src/app/pay-gov-mocks/landing-page/landing-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import {Router, ActivatedRoute, Params} from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { alphanumericValidator } from '../../application-forms/validators/alphanumeric-validation';
 
 @Component({
@@ -9,45 +9,42 @@ import { alphanumericValidator } from '../../application-forms/validators/alphan
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-
   applicationForm: FormGroup;
   successURL: any;
   failureURL: any;
 
-  constructor(public formBuilder: FormBuilder, private activatedRoute: ActivatedRoute,  private router: Router) {
-
+  constructor(public formBuilder: FormBuilder, private activatedRoute: ActivatedRoute, private router: Router) {
     this.applicationForm = this.formBuilder.group({
       paymentAmount: ['', [Validators.required, alphanumericValidator()]],
       cardholderName: ['', [Validators.required, alphanumericValidator()]],
-      cardholderBillingAddress  : ['', [Validators.required, alphanumericValidator()]],
-      cardholderBillingAddress2  : ['', [Validators.required, alphanumericValidator()]],
-      cardholderCity  : ['', [Validators.required, alphanumericValidator()]],
-      cardholderCountry  : ['', [Validators.required, alphanumericValidator()]],
-      cardholderState  : ['', [Validators.required, alphanumericValidator()]],
-      cardholderZip  : ['', [Validators.required, Validators.pattern(/(\d{5}|\d{9})/)]],
-      cardNumber  : ['', [Validators.required, Validators.pattern(/(\d{8,19})/)]],
-      cardExpirationMonth  : ['', [Validators.required]],
-      cardExpirationYear  : ['', [Validators.required]],
-      cardSecurityCode  : ['', [Validators.required, Validators.pattern(/(\d{3,4})/)]],
-      formName  : ['', [Validators.required, alphanumericValidator()]],
-      applicantName  : ['', [Validators.required, alphanumericValidator()]],
-      applicantEmailAddress  : ['', [Validators.required, alphanumericValidator()]],
-      selectedOption  : ['', [Validators.required, alphanumericValidator()]],
-      description  : ['', [Validators.required, alphanumericValidator()]],
-      amountOwed  : ['', [Validators.required, alphanumericValidator()]]
+      cardholderBillingAddress: ['', [Validators.required, alphanumericValidator()]],
+      cardholderBillingAddress2: ['', [Validators.required, alphanumericValidator()]],
+      cardholderCity: ['', [Validators.required, alphanumericValidator()]],
+      cardholderCountry: ['', [Validators.required, alphanumericValidator()]],
+      cardholderState: ['', [Validators.required, alphanumericValidator()]],
+      cardholderZip: ['', [Validators.required, Validators.pattern(/(\d{5}|\d{9})/)]],
+      cardNumber: ['', [Validators.required, Validators.pattern(/(\d{8,19})/)]],
+      cardExpirationMonth: ['', [Validators.required]],
+      cardExpirationYear: ['', [Validators.required]],
+      cardSecurityCode: ['', [Validators.required, Validators.pattern(/(\d{3,4})/)]],
+      formName: ['', [Validators.required, alphanumericValidator()]],
+      applicantName: ['', [Validators.required, alphanumericValidator()]],
+      applicantEmailAddress: ['', [Validators.required, alphanumericValidator()]],
+      selectedOption: ['', [Validators.required, alphanumericValidator()]],
+      description: ['', [Validators.required, alphanumericValidator()]],
+      amountOwed: ['', [Validators.required, alphanumericValidator()]]
     });
-
   }
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe((params: Params) => {
-      this.applicationForm.get('paymentAmount').setValue(params['']);
-      this.applicationForm.get('formName').setValue(params['']);
-      this.applicationForm.get('applicantName').setValue(params['']);
-      this.applicationForm.get('applicantEmailAddress').setValue(params['']);
-      this.applicationForm.get('selectedOption').setValue(params['']);
-      this.applicationForm.get('description').setValue(params['']);
-      this.applicationForm.get('amountOwed').setValue(params['']);
+      this.applicationForm.get('paymentAmount').setValue(params['paymentAmount']);
+      this.applicationForm.get('formName').setValue(params['formName']);
+      this.applicationForm.get('applicantName').setValue(params['applicantName']);
+      this.applicationForm.get('applicantEmailAddress').setValue(params['applicantEmailAddress']);
+      this.applicationForm.get('selectedOption').setValue(params['selectedOption']);
+      this.applicationForm.get('description').setValue(params['description']);
+      this.applicationForm.get('amountOwed').setValue(params['amountOwed']);
       this.successURL = params[''];
       this.failureURL = params[''];
     });
@@ -56,5 +53,4 @@ export class LandingPageComponent implements OnInit {
 
   cancelButtonClick() {}
   submitButtonClick() {}
-
 }
