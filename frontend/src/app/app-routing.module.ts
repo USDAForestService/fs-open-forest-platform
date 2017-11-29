404b7453b27abbc2eeaa5c9509a68b7b4415eb62
cwolf1038 <cwolf123456@yahoo.com>
2017-11-29 15:47:07 -0500

    Changed mocks-pay-gov to mock-pay-gov

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -103,7 +103,7 @@ const appRoutes: Routes = [
     data: { title: 'Christmas tree permit information' }
   },
   {
-    path: 'mocks-pay-gov',
+    path: 'mock-pay-gov',
     component: LandingPageComponent,
     data: { title: 'Complete your Christmas Tree permit transaction' }
   },
