a98d901d76d94651a4a7114f5a7b7bcdb54c7ffe
Will Sadler <wsadler@flexion.us>
2017-12-05 09:34:37 -0600

    Merge branch 'sprint-4-development' into 124_sidebar_consistency
    # Conflicts:
    #	frontend/src/app/sidebar/sidebar.component.spec.ts
    #	frontend/src/app/trees/forests/tree-guidelines/sidebar-view.component.spec.ts

--- frontend/src/app/trees/forests/tree-guidelines/sidebar-view.component.spec.ts
+++ frontend/src/app/trees/forests/tree-guidelines/sidebar-view.component.spec.ts
@@ -23,9 +23,6 @@ describe('SidebarViewComponent', () => {
     fixture = TestBed.createComponent(SidebarViewComponent);
     component = fixture.debugElement.componentInstance;
     component.forest = { forestName: 'Mt. Hood' };
-    component.bottom = '10px';
-    component.top = '20px';
-    component.position = 'absolute';
     fixture.detectChanges();
   });
 
@@ -39,34 +36,4 @@ describe('SidebarViewComponent', () => {
     })
   });
 
-  it('should set fixed position if top of the container is less than 20px', () => {
-    spyOn(document, 'getElementById').and.callFake(function() {
-      return {
-        value: 'test',
-        getBoundingClientRect() {
-          return { top: 19 };
-        }
-      };
-    });
-    component.track(new Event('scroll'));
-    expect(component.top).toEqual('140px');
-    expect(component.position).toEqual('fixed');
-  });
-
-  it('should set absolute position if top of the container is greater than 30px', () => {
-    spyOn(document, 'getElementById').and.callFake(function() {
-      return {
-        value: 'test',
-        getBoundingClientRect() {
-          return { top: 30 };
-        }
-      };
-    });
-    spyOn(window, 'innerHeight').and.callFake(function() {
-      return 50;
-    });
-    component.track(new Event('scroll'));
-    expect(component.top).toEqual('270px');
-    expect(component.position).toEqual('absolute');
-  });
 });
