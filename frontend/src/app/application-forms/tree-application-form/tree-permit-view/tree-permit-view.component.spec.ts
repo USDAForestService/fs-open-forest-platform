d6784a3abd02d475affc794c9d153ef48797beb5
Seth Alt <salt@flexion.us>
2017-12-12 13:50:11 -0600

    frontend unit test

--- frontend/src/app/application-forms/tree-application-form/tree-permit-view/tree-permit-view.component.spec.ts
+++ frontend/src/app/application-forms/tree-application-form/tree-permit-view/tree-permit-view.component.spec.ts
@@ -19,7 +19,8 @@ describe('TreePermitViewComponent', () => {
             provide: ActivatedRoute,
             useValue: {
               data: Observable.of({
-                forest: { id: '1', forestName: 'Mt Hood', orgStructureCode: '123', treeCost: 10, maxNumTrees: 5 }
+                forest: { id: '1', forestName: 'Mt Hood', orgStructureCode: '123', treeCost: 10, maxNumTrees: 5 },
+                permit: { permitId: '123', totalCost: 0, quantity: 0, emailAddress: '' }
               })
             }
           }
@@ -36,5 +37,13 @@ describe('TreePermitViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set forest on init', () => {
+    expect(component.forest.forestName).toEqual('Mt Hood');
+  });
+
+  it('should set permit on init', () => {
+    expect(component.permit.permitId).toEqual('123');
   });
 });
