60a8e84d92cc400de3e3403014685cd8defda382
cwolf1038 <cwolf123456@yahoo.com>
2017-12-14 12:36:14 -0500

    Adding functionality to print permit supplied by backend

--- frontend/src/app/application-forms/tree-application-form/tree-permit-view/tree-permit-view.component.ts
+++ frontend/src/app/application-forms/tree-application-form/tree-permit-view/tree-permit-view.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SecurityContext } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tree-permit-view',
@@ -9,8 +10,9 @@ import { Title } from '@angular/platform-browser';
 export class TreePermitViewComponent implements OnInit {
   forest: any;
   permit: any = { totalCost: 0, quantity: 0, emailAddress: '' };
+  image: any;
 
-  constructor(private route: ActivatedRoute, private titleService: Title) {}
+  constructor(private route: ActivatedRoute, private titleService: Title, private sanitizer: DomSanitizer) {}
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -21,6 +23,15 @@ export class TreePermitViewComponent implements OnInit {
           data.forest.forestName +
           ' National Forest | U.S. Forest Service Christmas Tree Permitting'
       );
+      this.image = this.sanitizer.bypassSecurityTrustHtml(this.permit.permitImage);
     });
+  }
+
+  printPermit() {
+    const printContents = document.getElementById('toPrint').innerHTML;
+    const originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
   }
 }
