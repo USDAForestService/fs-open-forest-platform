8ed975a540b2383d6f61fc4c7363bd03bc6b155e
cwolf1038 <cwolf123456@yahoo.com>
2018-01-16 13:36:20 -0500

    Replaced single quotes with double quotes to make sure contents of variable will still be passed through

--- .circleci/run-unauthenticated-e2e.sh
+++ .circleci/run-unauthenticated-e2e.sh
@@ -2,7 +2,7 @@
 docker-compose down
 
 #Rebuild server with PLATFORM set to something other than local to enable test to pass
-docker-compose build --build-arg PLATFORM='NOTCI' --build-arg VCAP_SERVICES='$VCAP_SERVICES' --build-arg VCAP_APPLICATION='$VCAP_APPLICATION' fs-intake-server
+docker-compose build --build-arg PLATFORM='NOTCI' --build-arg VCAP_SERVICES="$VCAP_SERVICES" --build-arg VCAP_APPLICATION="$VCAP_APPLICATION" fs-intake-server
 
 docker-compose run -e PLATFORM='not ci' fs-intake-frontend yarn run e2e:ci --environment docker --specs=e2e/unauthenticated/check-authentication.e2e-spec.ts;
 e2ereturncode=$?
