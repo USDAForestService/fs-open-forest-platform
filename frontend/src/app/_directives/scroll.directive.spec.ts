ace8fc27b670ec9efa441fac052af36b564f9eb8
Will Sadler <wsadler@flexion.us>
2018-01-22 11:21:04 -0600

    directive specs * title and scroll directive specs

---
+++ frontend/src/app/_directives/scroll.directive.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { TrackScrollDirective } from './scroll.directive';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `<div id="test" appTrackScroll>a div</div>`
+})
+class TestTrackScrollDirectiveComponent {
+}
+
+describe('Directive: TrackScrollDirective', () => {
+  let component: TestTrackScrollDirectiveComponent;
+  let fixture: ComponentFixture<TestTrackScrollDirectiveComponent>;
+  let inputEl: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TrackScrollDirective, TestTrackScrollDirectiveComponent]
+    });
+
+    fixture = TestBed.createComponent(TestTrackScrollDirectiveComponent);
+    component = fixture.componentInstance;
+    inputEl = fixture.debugElement.query(By.css('#test'));
+  });
+
+  it('should instantiate',() => {
+    inputEl.triggerEventHandler('scroll', null);
+    fixture.detectChanges();
+    expect(inputEl).toBeTruthy();
+  });
+});
