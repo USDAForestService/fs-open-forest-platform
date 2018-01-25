0fb9a015e7786e4152f7ef6b013cdb2782a05862
cwolf1038 <cwolf123456@yahoo.com>
2018-01-24 20:08:23 -0500

    Updated docker base image to try and fix spec errors
    * Updated frontend to be based on circle/node:6.12-browsers to ensure node and selenium are properly installed in docker container
    * Updated scripts to run commands as superuser within docker container

--- .circleci/install-dependencies-docker.sh
+++ .circleci/install-dependencies-docker.sh
@@ -1,2 +1,2 @@
-docker-compose run fs-intake-frontend yarn  
-docker-compose run fs-intake-server yarn && docker-compose run fs-intake-server yarn global add mocha && docker-compose run fs-intake-server yarn global add nyc
+docker-compose run fs-intake-frontend sudo yarn  
+docker-compose run fs-intake-server sudo yarn && docker-compose run fs-intake-server sudo yarn global add mocha && docker-compose run fs-intake-server sudo yarn global add nyc
--- .circleci/run-unauthenticated-e2e.sh
+++ .circleci/run-unauthenticated-e2e.sh
@@ -4,7 +4,7 @@ docker-compose down
 #Rebuild server with PLATFORM set to something other than local to enable test to pass
 docker-compose build --build-arg PLATFORM='NOTCI' --build-arg VCAP_SERVICES="$VCAP_SERVICES" --build-arg VCAP_APPLICATION="$VCAP_APPLICATION" fs-intake-server
 
-docker-compose run -e PLATFORM='not ci' fs-intake-frontend yarn run e2e:ci --environment docker --config unauth-protractor.conf.js;
+docker-compose run -e PLATFORM='not ci' fs-intake-frontend sudo yarn run e2e:ci --environment docker --config unauth-protractor.conf.js;
 e2ereturncode=$?
 
 if [[ $e2ereturncode = 0 ]]
