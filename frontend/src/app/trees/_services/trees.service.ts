ba89e46fcaf42424f9f99d83d2159b2577f588d6
Will Sadler <wsadler@flexion.us>
2018-01-30 14:18:15 -0600

    #102 seeder with environment override * revert seeders * add code to trees

--- frontend/src/app/trees/_services/trees.service.ts
+++ frontend/src/app/trees/_services/trees.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class TreesService {
@@ -9,6 +10,20 @@ export class TreesService {
   constructor(private http: HttpClient) {}
 
   getOne(id) {
-    return this.http.get(this.endpoint + id);
+    return this.http.get<any>(this.endpoint + id)
+      .map(data => {
+        if (data && !environment.production) {
+          if (environment.forestOverrides) {
+            for (let key in environment.forestOverrides) {
+              for (let attrname in environment.forestOverrides[key]) {
+                if ((data.id + '') === key) {
+                  data[attrname] = environment.forestOverrides[key][attrname];
+                }
+              }
+            }
+          }
+        }
+      return data;
+    });
   }
 }
