00199e788d13f894f00b2cac941f28805582e874
Will Sadler <wsadler@flexion.us>
2018-01-31 09:47:02 -0600

    357 remove fdescribe

--- frontend/e2e/authenticated/christmas-trees/xmas-tree-application.e2e-spec.ts
+++ frontend/e2e/authenticated/christmas-trees/xmas-tree-application.e2e-spec.ts
@@ -5,7 +5,7 @@ import {
 } from './xmas-tree-application.po';
 import { browser, element, by, Key } from 'protractor';
 
-fdescribe('Apply for a Christmas tree permit', () => {
+describe('Apply for a Christmas tree permit', () => {
   let page: ChristmasTreeForm;
   let confirmPage: ChristmasTreeOrderConfirmation;
   let prefilledPage: ChristmasTreeFormAfterCancel;
