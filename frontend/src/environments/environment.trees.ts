bc8a10038888c9fe2c9592d22c430f3c66a00d84
Will Sadler <wsadler@flexion.us>
2018-01-31 15:26:58 -0600

    testing prod env

--- frontend/src/environments/environment.trees.ts
+++ frontend/src/environments/environment.trees.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/',
+  apiUrl: 'https://fs-intake-api-trees.app.cloud.gov/',
   buildDate: 'Tue Jan 16 2018 17:06:56 GMT-0500 (EST)',
   version: '',
   forestOverrides: {
