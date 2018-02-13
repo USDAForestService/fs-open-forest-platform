099984bf0b860dea947131154ef366925e7b0478
Seth Alt <salt@flexion.us>
2018-02-13 16:14:43 -0600

    simplify trees component structure

--- frontend/src/app/trees/trees.module.ts
+++ frontend/src/app/trees/trees.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ApplicationsModule } from '../application-forms/applications.module';
-import { ContactInfoComponent } from './forests/tree-guidelines/contact-info/contact-info.component';
 import { ReportComponent } from './admin/report/report.component';
 import { ReportResultsComponent } from './admin/report/results/report-results.component';
 import { SharedModule } from '../_shared/shared.module';
 import { TreeGuidelinesComponent } from './forests/tree-guidelines/tree-guidelines.component';
 import { TreeCuttingDatesComponent } from './forests/tree-guidelines/tree-cutting-dates/tree-cutting-dates.component';
-import { TreeSpeciesComponent } from './forests/tree-guidelines/tree-selection/tree-species/tree-species.component';
-import { TreeToolsComponent } from './forests/tree-guidelines/tree-cutting/tree-tools/tree-tools.component';
-import { TripPlanningComponent } from './forests/tree-guidelines/trip-planning/trip-planning.component';
+import { TreeSpeciesComponent } from './forests/tree-guidelines/tree-species/tree-species.component';
 import { ForestService } from './_services/forest.service';
 import { TreeCuttingRulesComponent } from './forests/tree-guidelines/tree-cutting/tree-cutting-rules/tree-cutting-rules.component';
-import { TreeLocationsProhibitedComponent } from './forests/tree-guidelines/tree-locations/tree-locations-prohibited/tree-locations-prohibited.component';
-import { TreeLocationsAllowedComponent } from './forests/tree-guidelines/tree-locations/tree-locations-allowed/tree-locations-allowed.component';
 import { ForestFinderComponent } from './forests/forest-finder/forest-finder.component';
 import { RemovePuncPipe } from './forests/forest-finder/remove-punc.pipe';
 import { LineBreakFormatterPipe } from '../_pipes/line-break-formatter.pipe';
@@ -24,32 +19,25 @@ import { TreesAdminService } from './admin/trees-admin.service';
 
 @NgModule({
   declarations: [
-    ContactInfoComponent,
     ReportComponent,
     ReportResultsComponent,
     AdminSeasonDatesComponent,
     TreeCuttingRulesComponent,
     TreeGuidelinesComponent,
     TreeCuttingDatesComponent,
-    TreeLocationsProhibitedComponent,
-    TreeLocationsAllowedComponent,
     TreeSpeciesComponent,
-    TripPlanningComponent,
-    TreeToolsComponent,
     ForestFinderComponent,
     RemovePuncPipe,
     LineBreakFormatterPipe,
     ColumnizeArrayPipe
   ],
   exports: [
-    ContactInfoComponent,
     ReportComponent,
     ReportResultsComponent,
     TreeCuttingRulesComponent,
     TreeGuidelinesComponent,
     TreeCuttingDatesComponent,
     TreeSpeciesComponent,
-    TripPlanningComponent,
     LineBreakFormatterPipe,
     ColumnizeArrayPipe
   ],
