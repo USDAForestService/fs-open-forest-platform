7d4078e8d79660263082a7d0578717070718765b
Will Sadler <wsadler@flexion.us>
2018-02-22 17:44:33 -0600

    trying

--- .circleci/pa11y.sh
+++ .circleci/pa11y.sh
@@ -1,8 +1,9 @@
 docker-compose down
 docker-compose build --build-arg PLATFORM="$PLATFORM" --build-arg VCAP_SERVICES="$VCAP_SERVICES" --build-arg VCAP_APPLICATION="$VCAP_APPLICATION"  --build-arg SNYK_TOKEN="$SNYK_TOKEN" fs-intake-pa11y
 docker-compose build --build-arg PLATFORM="$PLATFORM" --build-arg VCAP_SERVICES="$VCAP_SERVICES" --build-arg VCAP_APPLICATION="$VCAP_APPLICATION"  --build-arg SNYK_TOKEN="$SNYK_TOKEN" fs-intake-server
-
-docker-compose run fs-intake-pa11y sudo yarn run pa11y
+sleep 60
+cd ../frontend
+pa11y-ci
 pa11yreturncode=$?
 if [[ $pa11yreturncode = 0 ]]
 then
@@ -11,4 +12,5 @@ else
   echo 'FAIL'
 fi
 cd ../docker
+docker-compose down
 exit $pa11yreturncode
