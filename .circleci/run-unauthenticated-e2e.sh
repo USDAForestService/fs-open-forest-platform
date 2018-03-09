42d6e8cd474e3b316ea559632c8c689cf1d054b1
Seth Alt <salt@flexion.us>
2018-03-09 14:32:56 -0600

    Remove vcap_application env var from unauthenticated script

--- .circleci/run-unauthenticated-e2e.sh
+++ .circleci/run-unauthenticated-e2e.sh
@@ -2,7 +2,7 @@
 docker-compose down
 
 #Rebuild server with PLATFORM set to something other than local to enable test to pass
-docker-compose build --build-arg PLATFORM='ci-unauthenticated' --build-arg VCAP_SERVICES="$VCAP_SERVICES" --build-arg VCAP_APPLICATION="$VCAP_APPLICATION" --build-arg AWS_CONFIG="$AWS_CONFIG" fs-intake-server
+docker-compose build --build-arg PLATFORM='ci-unauthenticated' --build-arg VCAP_SERVICES="$VCAP_SERVICES" -build-arg AWS_CONFIG="$AWS_CONFIG" fs-intake-server
 
 docker-compose run -e PLATFORM='ci-unauthenticated' fs-intake-frontend sudo yarn run e2e:ci --environment docker --config unauth-protractor.conf.js;
 e2ereturncode=$?
