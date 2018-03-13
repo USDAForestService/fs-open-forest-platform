4f7cc1e0278601b5118847b260a9f4dab1d04962
Seth Alt <salt@flexion.us>
2018-03-13 03:46:23 -0500

    run pa11y outside of docker

--- .circleci/pa11y.sh
+++ .circleci/pa11y.sh
@@ -1,4 +1,6 @@
-docker-compose run fs-intake-pa11y sudo yarn build-test-pa11y;
+cd frontend
+pwd
+npm run build-test-pa11y;
 pa11yreturncode=$?
 if [[ $pa11yreturncode = 0 ]]
 then
@@ -6,6 +8,5 @@ then
 else
   echo 'FAIL'
 fi
-cd ../docker
-docker-compose down
+
 exit $pa11yreturncode
