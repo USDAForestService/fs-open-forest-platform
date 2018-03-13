14e8e9705d4438fd30424e3e455b0e7997c93fa1
Shekar Pendem <shekar.pendem@gmail.com>
2018-03-13 15:17:49 -0400

    console log removed

--- frontend/src/app/error-pages/error-interceptor.service.ts
+++ frontend/src/app/error-pages/error-interceptor.service.ts
@@ -25,7 +25,6 @@ export class ErrorInterceptor implements HttpInterceptor {
 
     return handleObs
       .do(event => {
-        console.log('event=', event);
         if (event instanceof HttpResponse) {
           this.util.removeRequest();
         }
