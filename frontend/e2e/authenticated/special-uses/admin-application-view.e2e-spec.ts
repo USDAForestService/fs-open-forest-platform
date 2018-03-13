33d4337111c713e40629f55487856588163c61c9
Seth Alt <salt@flexion.us>
2018-03-13 02:34:23 -0500

    fix e2e

--- frontend/e2e/authenticated/special-uses/admin-application-view.e2e-spec.ts
+++ frontend/e2e/authenticated/special-uses/admin-application-view.e2e-spec.ts
@@ -62,7 +62,7 @@ describe('Admin applications page', () => {
     element(by.id('return-application-btn')).click();
     expect(element(by.css('#reason-for-action label')).getText()).toEqual('Why is this application being rejected?');
     element(by.id('reason-for-return')).sendKeys('test');
-    element(by.css('.reason-for-action-buttons .usa-button-primary')).click();
+    element(by.css('.reason-for-action-buttons .usa-button-red')).click();
     expect<any>(element(by.css('.usa-alert-body .usa-alert-text')).getText()).toEqual(
       'Permit application successfully rejected and an email with your message has been sent to the applicant.'
     );
