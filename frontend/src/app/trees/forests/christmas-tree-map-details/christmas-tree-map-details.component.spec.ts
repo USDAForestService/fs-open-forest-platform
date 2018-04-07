935311a4e01155ea2008d12e2906f301b4b152d7
Will Sadler <wsadler@flexion.us>
2018-04-07 17:21:36 -0500

    508 fixes * title for map details * content type for rules html attachment

--- frontend/src/app/trees/forests/christmas-tree-map-details/christmas-tree-map-details.component.spec.ts
+++ frontend/src/app/trees/forests/christmas-tree-map-details/christmas-tree-map-details.component.spec.ts
@@ -3,6 +3,9 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ChristmasTreeMapDetailsComponent } from './christmas-tree-map-details.component';
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 describe('ChristmasTreeMapDetailsComponent', () => {
   let component: ChristmasTreeMapDetailsComponent;
@@ -10,9 +13,21 @@ describe('ChristmasTreeMapDetailsComponent', () => {
 
   beforeEach(
     async(() => {
+      const mockSeasonNotOpenRoute = {
+        params: Observable.of({ id: 'id', mapId: 'mapId' }),
+        data: Observable.of({
+          forest: {
+            forestName: 'forest name'
+          }
+        })
+      };
+
       TestBed.configureTestingModule({
         declarations: [ChristmasTreeMapDetailsComponent],
         schemas: [NO_ERRORS_SCHEMA],
+        providers: [
+          { provide: ActivatedRoute, useValue: mockSeasonNotOpenRoute },
+          { provide: Title, useClass: Title }],
         imports: [HttpClientTestingModule, RouterTestingModule]
       }).compileComponents();
     })
@@ -26,5 +41,10 @@ describe('ChristmasTreeMapDetailsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the title using forest and map id',() => {
+    const titleService = TestBed.get(Title);
+    expect(titleService.getTitle()).toBe('Map details for forest name mapId cutting area or ranger district | U.S. Forest Service Christmas Tree Permitting');
   });
 });
--- frontend/src/app/trees/forests/christmas-tree-map-details/christmas-tree-map-details.component.ts
+++ frontend/src/app/trees/forests/christmas-tree-map-details/christmas-tree-map-details.component.ts
@@ -31,7 +31,5 @@ export class ChristmasTreeMapDetailsComponent implements OnInit {
       }
     });
 
-
-
   }
 }
