a6ac0f70563e16d7236974ddda89c643af96f90d
Laura Gerhardt <laura.gerhardt@gsa.gov>
2018-04-25 09:17:03 -0400

    adjust pa11y build

--- .circleci/pa11y.sh
+++ .circleci/pa11y.sh
@@ -1,7 +1,10 @@
+echo 'BUILDING Pa11y DOCKER IMAGE'
 docker-compose build fs-intake-pa11y
 docker-compose up fs-intake-pa11y &
 sleep 300
+
 cd ../frontend
+echo 'STARTING Pa11y TESTS'
 sudo npm run pa11y
 pa11yreturncode=$?
 if [[ $pa11yreturncode = 0 ]]
