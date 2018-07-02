e2a59d2b75cd48b402a00fe0f48c6a7ce8e04777
Laura Gerhardt <laura.gerhardt@gsa.gov>
2018-07-02 16:20:40 -0400

    fix snyk combo uploads

--- .circleci/store-snyk.sh
+++ .circleci/store-snyk.sh
@@ -1,8 +1,8 @@
 sudo apt-get install -y awscli
 export DATE_STRING=`date +"%m-%d-%Y-%H"`
-export SNYK_REPORT_SERVER=snyk-${CIRCLE_PR_REPONAME}-server-${DATE_STRING}.csv
-export SNYK_REPORT_FRONTEND=snyk-${CIRCLE_PR_REPONAME}-frontend-${DATE_STRING}.csv
-export SNYK_REPORT=snyk-${CIRCLE_PR_REPONAME}-${DATE_STRING}.csv
+export SNYK_REPORT_SERVER=snyk-${CIRCLE_PROJECT_REPONAME}-server-${DATE_STRING}.csv
+export SNYK_REPORT_FRONTEND=snyk-${CIRCLE_PROJECT_REPONAME}-frontend-${DATE_STRING}.csv
+export SNYK_REPORT=snyk-${CIRCLE_PROJECT_REPONAME}-${DATE_STRING}.csv
 
 mkdir -p a=rw snyk
 echo "running frontend snyk"
@@ -20,7 +20,7 @@ sudo cat ../snyk/snyk-server.json  \
 | jq -r '["App","Package", "CVE", "CWE", "Severity", "CVSSScore", "Title", "Disclosure Time", "Description", "Library Path Parent", "Library Path Child"], (.vulnerabilities[] | ["server", .packageName, .identifiers.CVE[0], .identifiers.CWE[0], .severity, .cvssScore, .title,.disclosureTime, .description, .from[1], .from[2]] )| @csv' >  \
 "../snyk/${SNYK_REPORT_SERVER}"
 cd ..
-cat "${SNYK_REPORT_FRONTEND}" <(tail +2 "${SNYK_REPORT_SERVER}") > "snyk/${SNYK_REPORT}"
+cat ./snyk/*.csv > snyk/"${SNYK_REPORT}"
 echo "uploading snyk"
 
 export STORE_BUCKET=`echo "${LOG_S3}" | jq -r .bucket`
