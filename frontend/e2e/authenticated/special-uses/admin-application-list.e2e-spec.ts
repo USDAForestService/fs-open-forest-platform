2003fc69c0aa0c2376e3c895df0acd3f9861f328
Laura Gerhardt <laura.gerhardt@gsa.gov>
2018-07-05 15:25:06 -0400

    attempt to fix detail view cancel ci test

--- frontend/e2e/authenticated/special-uses/admin-application-list.e2e-spec.ts
+++ frontend/e2e/authenticated/special-uses/admin-application-list.e2e-spec.ts
@@ -62,7 +62,8 @@ describe('Apply for a ', () => {
   });
 
   it('should cancel an application when the cancellation button is clicked', () => {
-    element(by.cssContainingText('.cancel-button-user', 'Cancel')).first().click();
+    const cancelButtons = element.all(by.cssContainingText('.cancel-button-user', 'Cancel'));
+    cancelButtons.first().click();
     browser.sleep(100);
     expect<any>(element(by.css('app-root usa-alert-text')).getText()).toEqual('Permit application was successfully cancelled.');
   });
