1fc538bf8916a9407f7b2cac7ce098329176d24a
Laura Gerhardt <laura.gerhardt@gsa.gov>
2018-10-22 11:27:29 -0400

    remove sequelize string operators

--- server/.sequelize.js
+++ server/.sequelize.js
@@ -7,44 +7,6 @@ const logger = require('./src/services/logger.es6');
 const dbParams = url.parse(process.env.DATABASE_URL, true);
 const dbAuth = dbParams.auth.split(':');
 
-const Operators = Sequelize.Op;
-const operatorsAliases = {
-  $eq: Operators.eq,
-  $ne: Operators.ne,
-  $gte: Operators.gte,
-  $gt: Operators.gt,
-  $lte: Operators.lte,
-  $lt: Operators.lt,
-  $not: Operators.not,
-  $in: Operators.in,
-  $notIn: Operators.notIn,
-  $is: Operators.is,
-  $like: Operators.like,
-  $notLike: Operators.notLike,
-  $iLike: Operators.iLike,
-  $notILike: Operators.notILike,
-  $regexp: Operators.regexp,
-  $notRegexp: Operators.notRegexp,
-  $iRegexp: Operators.iRegexp,
-  $notIRegexp: Operators.notIRegexp,
-  $between: Operators.between,
-  $notBetween: Operators.notBetween,
-  $overlap: Operators.overlap,
-  $contains: Operators.contains,
-  $contained: Operators.contained,
-  $adjacent: Operators.adjacent,
-  $strictLeft: Operators.strictLeft,
-  $strictRight: Operators.strictRight,
-  $noExtendRight: Operators.noExtendRight,
-  $noExtendLeft: Operators.noExtendLeft,
-  $and: Operators.and,
-  $or: Operators.or,
-  $any: Operators.any,
-  $all: Operators.all,
-  $values: Operators.values,
-  $col: Operators.col
-};
-
 const dbConfig = {
   database: dbParams.pathname.split('/')[1],
   username: dbAuth[0],
@@ -57,7 +19,7 @@ const dbConfig = {
   logging: function(sql, sequelizeObject) {
        logger.info(`SEQUELIZE: ${sql}`);
   },
-  operatorsAliases: operatorsAliases
+  operatorsAliases: false
 };
 
 if (dbParams.hostname !== 'localhost' && dbParams.hostname !== 'fs-intake-postgres') {
