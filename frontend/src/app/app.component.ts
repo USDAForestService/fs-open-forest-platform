823d4f620b5e187f594b052e30dce5c3dea4213b
Laura Gerhardt <laura.gerhardt@gsa.gov>
2018-12-11 17:32:14 -0500

    install ga types and update ga send

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -46,8 +46,8 @@ export class AppComponent implements OnInit {
           localStorage.removeItem('showLoggedIn');
           this.setStatus();
         }
-        (<any>window).ga('set', 'page', event.urlAfterRedirects);
-        (<any>window).ga('send', 'pageview');
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
       }
     });
   }
