19c4a047f1081cf5baf8b68c52d5950ab5f85ae2
David Corwin <david.corwin@gsa.gov>
2019-04-12 13:15:27 -0700

    489 verify email (#596)

---
+++ frontend/src/app/application-forms/validators/email-confirmation-validation.ts
@@ -0,0 +1,9 @@
+import { FormGroup, ValidatorFn, ValidationErrors } from '@angular/forms';
+
+export function emailConfirmationValidator(emailControl: string, confirmControl: string): ValidatorFn {
+  return (control: FormGroup): ValidationErrors | null => {
+    const email = control.get(emailControl);
+    const confirm = control.get(confirmControl);
+    return email && confirm && email.value !== confirm.value ? { 'emailConfirmationRequirement': true } : null;
+  };
+}
