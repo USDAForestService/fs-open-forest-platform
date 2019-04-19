2cfd0d779c57c64de75a9919ccb17c12ef3fe655
Michael Laney <mtlaney@gmail.com>
2019-04-19 10:53:46 -0400

    change test for h2

--- frontend/e2e/authenticated/special-uses/application-temp-outfitters.e2e-spec.ts
+++ frontend/e2e/authenticated/special-uses/application-temp-outfitters.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Apply for a temp outfitters permit', () => {
 
   it('should display the permit name in the header', () => {
     page.navigateTo();
-    expect<any>(element(by.css('app-root h1')).getText()).toEqual('Apply for a temporary outfitters permit with Open Forest.');
+    expect<any>(element(by.css('app-root h2')).getText()).toEqual('Apply for a temporary outfitters permit with Open Forest.');
   });
 
   it('should not show errors by default', () => {
