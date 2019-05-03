1c2312e808b59e8de4b683777ff639fb841bf26d
David Corwin <david.corwin@gsa.gov>
2019-05-03 14:35:32 -0700

    Another try

--- .circleci/run-e2e.sh
+++ .circleci/run-e2e.sh
@@ -1,11 +1,33 @@
 export NODE_ENV=test
 
+SUITE=""
+if [ $CIRCLE_NODE_TOTAL -ge 3 ]
+then
+  case $CIRCLE_NODE_INDEX in
+    0)
+      SUITE="xmas"
+      ;;
+    1)
+      SUITE="su"
+      ;;
+    2)
+      SUITE="unauthenticated"
+      ;;
+  esac
+fi
+
+PARAMS=""
+if [ -n "$SUITE" ]
+then
+  PARAMS="-- --suite=$SUITE"
+fi
+
 cd server;
 npm start &
 serverid=$!
 sleep 1
 cd ../frontend;
-sudo npm run e2e -- $1;
+sudo npm run e2e $PARAMS;
 
 e2ereturncode=$?
 
