76d8ef7850ead47671219eaf822f5eef1b50252e
David Corwin <david.corwin@gsa.gov>
2019-05-03 14:05:04 -0700

    Another e2e attempt

--- frontend/development-configurations/protractor.conf.js
+++ frontend/development-configurations/protractor.conf.js
@@ -52,10 +52,10 @@ exports.config = {
     });
   },
   suites: {
-    'docker-smoke-test': '../e2e/authenticated/special-uses/noncommercial-learn-more.e2e-spec.ts',
-    'app-temp': '../e2e/authenticated/special-uses/application-temp-outfitters.e2e-spec.ts',
-    'app-xmas': '../e2e/authenticated/christmas-trees/xmas-tree-application.e2e-spec.ts',
-    'unauthenticated': ['../e2e/unauthenticated/**/*.e2e-spec.ts']
+    'docker-smoke-test': '../e2e/unauthenticated/**/*spec.ts',
+    'su': '../e2e/authenticated/special-uses/**/*spec.ts',
+    'xmas': '../e2e/authenticated/christmas-trees/**/*spec.ts',
+    'unauthenticated': '../e2e/unauthenticated/**/*spec.ts'
   },
   chromeDriver: !isDocker && process.env['OPEN_FOREST_CHROME_DRIVER'],
 };
