8b23f589089f3b6c5077c5eab017b1165a6ae64f
brian davidson <brian.davidson.1981@gmail.com>
2019-06-26 12:08:27 -0400

    updated the migration file to account for pre-existing data

--- server/dba/migrations/37-addPurchaseDate.js
+++ server/dba/migrations/37-addPurchaseDate.js
@@ -4,7 +4,9 @@ const TABLE_NAME = "christmasTreesPermits";
 
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.addColumn(TABLE_NAME, 'purchase_date', { type: Sequelize.DATE, allowNull: true });
+    return queryInterface.addColumn(TABLE_NAME, 'purchase_date', { type: Sequelize.DATE, allowNull: true }).then(() => {
+      queryInterface.sequelize.query('UPDATE "christmasTreesPermits" SET purchase_date = updated')
+    });
   },
   down: function(queryInterface, Sequelize) {
     return queryInterface.removeColumn(TABLE_NAME, 'purchase_date');
