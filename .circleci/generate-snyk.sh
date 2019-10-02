4212f3ef3823ab6687229e7d0b7012e87f858224
Abdul Qureshi <abdul.qureshi@usda.gov>
2019-10-02 13:21:00 -0400

    snyk-email-updates-fix

--- .circleci/generate-snyk.sh
+++ .circleci/generate-snyk.sh
@@ -6,16 +6,16 @@ export SNYK_REPORT_FRONTEND=snyk-${CIRCLE_PROJECT_REPONAME}-frontend-${DATE_STRI
 mkdir -p a=rw snyk
 echo "running frontend snyk"
 sudo npm install -g snyk
-cd frontend
-snyk test --json > ../snyk/snyk-frontend.json
-sudo cat ../snyk/snyk-frontend.json  \
+cd ../frontend
+snyk test --json > ../.circleci/snyk/snyk-frontend.json
+sudo cat ../.circleci/snyk/snyk-frontend.json  \
 | jq -r '["App","Package", "CVE", "CWE", "Severity", "CVSSScore", "Title", "Disclosure Time", "Description", "Library Path Parent", "Library Path Child"], (.vulnerabilities[] | ["frontend", .packageName, .identifiers.CVE[0], .identifiers.CWE[0], .severity, .cvssScore, .title,.disclosureTime, .description, .from[1], .from[2]] )| @csv' >  \
 "../${SNYK_DIR}/${SNYK_REPORT_FRONTEND}"
 echo "running server snyk"
 cd ../server
 npm run snyk-protect
-snyk test --json > ../snyk/snyk-server.json
-sudo cat ../snyk/snyk-server.json  \
+snyk test --json > ../.circleci/snyk/snyk-server.json
+sudo cat ../.circleci/snyk/snyk-server.json  \
 | jq -r '(.vulnerabilities[] | ["server", .packageName, .identifiers.CVE[0], .identifiers.CWE[0], .severity, .cvssScore, .title,.disclosureTime, .description, .from[1], .from[2]] )| @csv' >  \
 "../${SNYK_DIR}/${SNYK_REPORT_SERVER}"
 cd ..
