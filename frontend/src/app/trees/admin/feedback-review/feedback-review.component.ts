32b705a4652cced9aafa36d67f03d40e2e080eae
brian davidson <brian.davidson.1981@gmail.com>
2019-10-17 15:53:46 -0400

    840 - removed unused deps. style updates for smaller display

--- frontend/src/app/trees/admin/feedback-review/feedback-review.component.ts
+++ frontend/src/app/trees/admin/feedback-review/feedback-review.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, Input, OnChanges, Injectable } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Angular2Csv } from 'angular2-csv';
 import { FeedbackService } from '../../_services/feedback.service';
-import { Title } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import * as moment from 'moment/moment';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-feedback-review',
@@ -18,13 +17,12 @@ export class AdminFeedbackReviewComponent implements OnInit {
   constructor(
     private http: HttpClient,
     private service: FeedbackService,
-    private titleService: Title,
     private router: Router,
-    private route: ActivatedRoute,
   ) {
   }
 
   ngOnInit() {
+    // get all the feedback data and format the date values
     this.service.getAll().subscribe(res => {
       for (const entry in res) {
         if (res[entry]) {
