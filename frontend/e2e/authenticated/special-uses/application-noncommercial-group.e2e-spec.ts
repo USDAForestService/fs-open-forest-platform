62c1119caa7b91f6313be60fe3368db511f2cd75
Michael Laney <mtlaney@gmail.com>
2019-11-04 13:44:43 -0500

    update test

--- frontend/e2e/authenticated/special-uses/application-noncommercial-group.e2e-spec.ts
+++ frontend/e2e/authenticated/special-uses/application-noncommercial-group.e2e-spec.ts
@@ -47,6 +47,7 @@ describe('Apply for a noncommercial group use permit', () => {
 
   it('should submit an application as individual with only the required fields populated', () => {
     page.navigateTo();
+    element(by.id('accept-pii')).click();
     element(by.css('#person-primary-name .primary-permit-holder-first-name')).sendKeys('Micky');
     element(by.css('#person-primary-name .primary-permit-holder-last-name')).sendKeys('Watson');
     element(by.css('#person-primary-address .primary-permit-holder-address')).sendKeys('933 Easy St');
