4bb8981bd7b8fa47393b410c247703e1ce54b1f1
brian davidson <brian.davidson.1981@gmail.com>
2019-11-26 12:57:10 -0500

    1040 - added dummy page for printing permits so no 404 page would show

---
+++ frontend/src/app/print-permit-dummy-page/dummy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy',
+  templateUrl: './dummy.component.html'
+})
+export class DummyComponent {
+  constructor(
+  ) {
+  }
+}
