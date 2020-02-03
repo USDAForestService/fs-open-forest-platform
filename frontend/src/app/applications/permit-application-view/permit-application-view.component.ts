90e0e5d4149982cfab74b298c8d451bc6e2e2130
Dmac26 <dylan.mcafee@usda.gov>
2020-02-03 14:11:07 -0600

    Attempt fix failed unit test_10

--- frontend/src/app/applications/permit-application-view/permit-application-view.component.ts
+++ frontend/src/app/applications/permit-application-view/permit-application-view.component.ts
@@ -31,6 +31,8 @@ export class PermitApplicationViewComponent implements OnInit {
   };
 
   constructor(
+    private formBuilder: FormBuilder,
+    public afs: ApplicationFieldsService,
     public alertService: AlertService,
     public applicationService: ApplicationService,
     private route: ActivatedRoute,
