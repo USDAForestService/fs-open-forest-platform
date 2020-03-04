a179a40d0d80a112b6ce8e85289d265bfe31fd5b
kilara77 <kilara77@gmail.com>
2020-03-04 12:07:10 -0500

    Update createrelease.sh

--- createrelease.sh
+++ createrelease.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pwd
-hub release create -a sonarqubereports/sonarqubeissuesreport.xlsx -a sonarqubereports/sonarqubeanalysisreport.docx -m Jenkins-${GIT_BRANCH}-Build-$BUILD_NUMBER Jenkins-${GIT_BRANCH}-Build-$BUILD_NUMBER
+/home/Jenkins/.gvm/pkgsets/go1.11/global/bin/hub release create -a sonarqubereports/sonarqubeissuesreport.xlsx -a sonarqubereports/sonarqubeanalysisreport.docx -m Jenkins-${GIT_BRANCH}-Build-$BUILD_NUMBER Jenkins-${GIT_BRANCH}-Build-$BUILD_NUMBER
 pwd
