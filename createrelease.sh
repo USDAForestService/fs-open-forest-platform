76cee7f98bb745649e7cecb2ba3c03a2c48370e2
kilara77 <kilara77@gmail.com>
2020-03-04 12:05:10 -0500

    Update createrelease.sh

--- createrelease.sh
+++ createrelease.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 pwd
 hub release create -a sonarqubereports/sonarqubeissuesreport.xlsx -a sonarqubereports/sonarqubeanalysisreport.docx -m Jenkins-${GIT_BRANCH}-Build-$BUILD_NUMBER Jenkins-${GIT_BRANCH}-Build-$BUILD_NUMBER
 pwd
