4056ddf754fd3ee6f203d62eeda62f702593c71f
Michael Laney <mtlaney@gmail.com>
2020-03-17 11:03:57 -0400

    consolidating code to match

--- frontend/src/app/main-landing/main-landing.component.ts
+++ frontend/src/app/main-landing/main-landing.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-landing',
   templateUrl: './main-landing.component.html',
-  styleUrls: ['./main-landing.component.scss']
 })
 export class MainLandingComponent implements OnInit {
 
