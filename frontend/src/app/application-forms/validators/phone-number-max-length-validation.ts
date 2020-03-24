d199051bd06ab0cacdfbbc3e42924aa4135f9bad
Dmac26 <dylanmc21@gmail.com>
2020-03-24 12:58:35 -0500

    update variable name with correct spelling

--- frontend/src/app/application-forms/validators/phone-number-max-length-validation.ts
+++ frontend/src/app/application-forms/validators/phone-number-max-length-validation.ts
@@ -4,12 +4,12 @@ export function phoneNumberMaxValidator(): ValidatorFn {
   return (control: FormControl): { [key: string]: any } => {
     const val = control.value;
     if (val && val.length) {
-      const phoneNumbrRegex = /^[0-9-]{2,}[0-9]$/;
+      const phoneNumberRegex = /^[0-9-]{2,}[0-9]$/;
       let isTenOrLess = false;
       if (val.length > 10) {
         isTenOrLess = true;
       }
-      const valid = phoneNumbrRegex.test(val) && isTenOrLess === false;
+      const valid = phoneNumberRegex.test(val) && isTenOrLess === false;
       return valid ? null : { phoneNumberMaxRequirement: true };
     } else {
       return null;
