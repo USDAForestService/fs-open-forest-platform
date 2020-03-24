eec5aabed26a840b139539aa7b56988adcce53ae
Dmac26 <dylanmc21@gmail.com>
2020-03-24 11:36:14 -0500

    Make phoneNumberRegex variable name more explicit

--- frontend/src/app/application-forms/validators/phone-number-max-length-validation.ts
+++ frontend/src/app/application-forms/validators/phone-number-max-length-validation.ts
@@ -4,12 +4,12 @@ export function phoneNumberMaxValidator(): ValidatorFn {
   return (control: FormControl): { [key: string]: any } => {
     const val = control.value;
     if (val && val.length) {
-      const phoneNmbrRegex = /^[0-9-]{2,}[0-9]$/;
+      const phoneNumbrRegex = /^[0-9-]{2,}[0-9]$/;
       let isTenOrLess = false;
       if (val.length > 10) {
         isTenOrLess = true;
       }
-      const valid = phoneNmbrRegex.test(val) && isTenOrLess === false;
+      const valid = phoneNumbrRegex.test(val) && isTenOrLess === false;
       return valid ? null : { phoneNumberMaxRequirement: true };
     } else {
       return null;
