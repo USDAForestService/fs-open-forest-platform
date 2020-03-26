f47baa63c0b3437c7161db21c5f53661a5217caa
kilara77 <kilara77@gmail.com>
2020-03-26 09:25:27 -0400

    Update karma.conf.js

--- frontend/development-configurations/karma.conf.js
+++ frontend/development-configurations/karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 const isDocker = require('is-docker')();
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
@@ -29,11 +30,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+   browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
-      ChromeHeadless1024X768: {
+      ChromeHeadlessNoSandbox: {
         base: "ChromeHeadless",
-        flags: isDocker ? ["--window-size=1024,768", "--disable-gpu", "--no-sandbox", "--disable-setuid-sandbox"] : ["--window-size=1024,768"]
+        flags: ['--no-sandbox']
       }
     },
     singleRun: false
