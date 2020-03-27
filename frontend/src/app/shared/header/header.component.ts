db8743f7be0fb3848c237d68ce041a21566f323a
Dmac26 <dylanmc21@gmail.com>
2020-03-27 16:16:20 -0500

    Update .ts file to complete 1165

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,8 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-page-header',
   templateUrl: './header.component.html'
 })
-export class PageHeaderComponent {}
+export class PageHeaderComponent {
+  browserName: string;
+  warningMessage: string;
+
+  constructor(public router: Router,
+    private meta: Meta) {
+    this.warningMessage = ''
+  }
+
+  getBrowserName() {
+    const  userAgent = navigator.userAgent;
+    let browserInfo = userAgent.match (/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    let parsedBrowserInfo;
+
+        if (/trident/i.test(browserInfo[1])) {
+
+          parsedBrowserInfo = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
+
+          return { name: 'IE', version: (parsedBrowserInfo[1] || '') };
+
+        }
+
+    if (browserInfo[1] === 'Chrome') {
+
+        parsedBrowserInfo = userAgent.match (/\bOPR|Edge\/(\d+)/);
+
+        if (parsedBrowserInfo != null)   {
+
+            return { name: 'Opera', version: parsedBrowserInfo[1] };
+
+          }
+
+        }
+
+      browserInfo = browserInfo[2] ? [browserInfo[1], browserInfo[2]] : [navigator.appName, navigator.appVersion, '-?'];
+
+    if (( parsedBrowserInfo = userAgent.match(/version\/(\d+)/i)) != null) {
+
+      browserInfo.splice(1, 1, parsedBrowserInfo[1]);
+
+    }
+
+     this.browserName = browserInfo[0];
+
+     return this.browserName;
+
+ }
+
+}
