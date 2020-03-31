46aabd813240589f88fc74995bd41432eaf417a9
kilara77 <kilara77@gmail.com>
2020-03-31 15:16:15 -0400

    Delete clean-s3-bucket.sh

--- .circleci/clean-s3-bucket.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install -y awscli
-export BUCKET=`echo "${AWS_CONFIG}" | jq -r .s3[0].credentials.bucket`
-export AWS_ACCESS_KEY_ID=`echo "${AWS_CONFIG}" | jq -r .s3[0].credentials.access_key_id`
-export AWS_SECRET_ACCESS_KEY=`echo "${AWS_CONFIG}" | jq -r .s3[0].credentials.secret_access_key`
-export AWS_DEFAULT_REGION=`echo "${AWS_CONFIG}" | jq -r .s3[0].credentials.region`
-aws s3 rm s3://"${BUCKET}" --recursive
