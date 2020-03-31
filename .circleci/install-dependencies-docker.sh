241d0fdd642a2136f98718614334f4bffd92dbf1
kilara77 <kilara77@gmail.com>
2020-03-31 15:16:38 -0400

    Delete install-dependencies-docker.sh

--- .circleci/install-dependencies-docker.sh
+++
@@ -1,4 +0,0 @@
-docker-compose run fs-intake-frontend sudo npm install
-docker-compose run fs-intake-server npm install &&
-  docker-compose run fs-intake-server npm install -g mocha &&
-  docker-compose run fs-intake-server npm install -g nyc
