3652b8f55a225b688f06ffdc4eea8c9a725902b2
Dmac26 <dylanmc21@gmail.com>
2020-04-03 10:58:57 -0500

    NCGU titles: consistent wording & capitalization

--- frontend/src/app/application-forms/application-noncommercial-group/noncommercial-learn-more.component.ts
+++ frontend/src/app/application-forms/application-noncommercial-group/noncommercial-learn-more.component.ts
@@ -17,7 +17,7 @@ export class NoncommercialLearnMoreComponent {
   ) {
     this.meta.addTag({
       name: 'description',
-      content: 'Learn more about how to use Open Forest to apply for non-commercial group use permits on the Mt. Baker-Snoqualmie National Forest.'
+      content: 'Learn more about how to use Open Forest to apply for Non-Commercial Group Use permits on the Mt. Baker-Snoqualmie National Forest.'
     });
     this.forest = this.specialUseInfoService.getOne('0605');
     this.items = [
@@ -27,7 +27,7 @@ export class NoncommercialLearnMoreComponent {
         sectionCopy: `
         <ul>
         <li>According to the regulation, permits are required for 75 or more people. However, some forest may have special considerations, contact your local forest if you have questions.</li>
-        <li>Authorization for non-commercial groups to use national Forest Service land.</li>
+        <li>Authorization for Non-Commercial Groups to use national Forest Service land.</li>
         <li>Required for groups of 75 or more people (participants and spectators).</li>
         <li>Important to minimize resource and environmental impacts on forest lands.</li>
         <li>Valid for the event or activity only.</li>
@@ -42,7 +42,7 @@ export class NoncommercialLearnMoreComponent {
         <ul>
         <li>Must be at least 21 years old.</li>
         <li>Must sign the permit on behalf of the group (but will not be held personally responsible for the group’s actions).</li>
-        <li>Insurance is NOT required for non-commercial permits.</li>
+        <li>Insurance is NOT required for Non-Commercial Group Use permits.</li>
         </ul>`
       },
       {
@@ -58,7 +58,7 @@ export class NoncommercialLearnMoreComponent {
         primarily if it violates public safety or public health laws or regulations.
         If the permit is denied, an officer is required to explain the reasons to the applicant in writing.</p>
 
-        <p>For more information about non-commercial group use and regulations, <a href="https://www.fs.fed.us/specialuses/special_non_com_uses.shtml">visit our FAQ page</a></p>`
+        <p>For more information about Non-Commercial Group Use permits and regulations, <a href="https://www.fs.fed.us/specialuses/special_non_com_uses.shtml">visit our FAQ page</a></p>`
       },
       {
         sectionName: 'Contact us',
@@ -66,7 +66,7 @@ export class NoncommercialLearnMoreComponent {
         sectionCopy: `
         <p>If you'd like to contact the special use permit administration staff at the Forest, please use a method listed below.</p>
         <div class="contact-details">
-          <h3>Non-commercial contact</h3>
+          <h3>Non-Commercial Group Use contact</h3>
           <p class="title-description">
             Sue Sherman-Biery<br>
             <span class="italic">Special use administrator</span>
