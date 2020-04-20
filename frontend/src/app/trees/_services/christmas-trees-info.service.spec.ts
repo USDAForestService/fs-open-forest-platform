ec5116486ad6b6cf933f92db77b71902b6dce476
Michael Laney <mtlaney@gmail.com>
2020-04-20 16:22:59 -0400

    remove needless test

--- frontend/src/app/trees/_services/christmas-trees-info.service.spec.ts
+++ frontend/src/app/trees/_services/christmas-trees-info.service.spec.ts
@@ -116,9 +116,6 @@ describe('ChristmasTreesInfoService', () => {
       mdRequestUrls.forEach(mdRequestUrl => mdRequests.push(httpMock.expectOne(mdRequestUrl)));
       mdRequests.forEach(request => request.flush('blah'));
 
-      const jsonRequest = httpMock.expectOne('/assets/config/christmasTreesForests-arp.json');
-      jsonRequest.flush(forestJSON);
-
       httpMock.verify();
     })
   );
