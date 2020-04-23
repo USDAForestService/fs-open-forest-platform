2540b4da741f1d0d001b6b26c0dbd4ebb5c30390
Michael Laney <mtlaney@gmail.com>
2020-04-22 20:25:30 -0400

    remove typo

--- server/dba/migrations/40-addState.js
+++ server/dba/migrations/40-addState.js
@@ -3,11 +3,8 @@
 const TABLE_NAME = "christmasTreesForests";
 
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.addColumn(TABLE_NAME, 'state', { 
-        type: Sequelize.STRING,
-         allowNull: true
-        }),
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.addColumn(TABLE_NAME, 'state', { type: Sequelize.STRING, allowNull: true });
   },
   down: function(queryInterface, Sequelize) {
     return queryInterface.removeColumn(TABLE_NAME, 'state');
