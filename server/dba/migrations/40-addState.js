755174d1e61a57a90fd8de475c7a390da2c386d4
Michael Laney <mtlaney@gmail.com>
2020-04-22 20:03:10 -0400

    change to string

--- server/dba/migrations/40-addState.js
+++ server/dba/migrations/40-addState.js
@@ -4,9 +4,10 @@ const TABLE_NAME = "christmasTreesForests";
 
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.addColumn(TABLE_NAME, 'state', { type: Sequelize.DATE, allowNull: true }).then(() => {
-      queryInterface.sequelize.query('UPDATE "christmasTreesForests" SET state = updated')
-    });
+    return queryInterface.addColumn(TABLE_NAME, 'state', { 
+        type: Sequelize.STRING,
+         allowNull: true
+        }),
   },
   down: function(queryInterface, Sequelize) {
     return queryInterface.removeColumn(TABLE_NAME, 'state');
