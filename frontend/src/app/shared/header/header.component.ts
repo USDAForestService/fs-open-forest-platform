f56c61f24a6b89318c4261b17333230578cb84e9
Dmac26 <dylanmc21@gmail.com>
2020-04-28 15:29:30 -0500

    Make syntax more consistent with the rest of app

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -20,37 +20,20 @@ export class PageHeaderComponent {
     let parsedBrowserInfo;
 
         if (/trident/i.test(browserInfo[1])) {
-
           parsedBrowserInfo = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
-
           return { name: 'IE', version: (parsedBrowserInfo[1] || '') };
-
         }
-
-    if (browserInfo[1] === 'Chrome') {
-
+        if (browserInfo[1] === 'Chrome') {
         parsedBrowserInfo = userAgent.match (/\bOPR|Edge\/(\d+)/);
-
         if (parsedBrowserInfo != null)   {
-
             return { name: 'Opera', version: parsedBrowserInfo[1] };
-
           }
-
         }
-
       browserInfo = browserInfo[2] ? [browserInfo[1], browserInfo[2]] : [navigator.appName, navigator.appVersion, '-?'];
-
-    if (( parsedBrowserInfo = userAgent.match(/version\/(\d+)/i)) != null) {
-
-      browserInfo.splice(1, 1, parsedBrowserInfo[1]);
-
+      if (( parsedBrowserInfo = userAgent.match(/version\/(\d+)/i)) != null) {
+        browserInfo.splice(1, 1, parsedBrowserInfo[1]);
     }
-
      this.browserName = browserInfo[0];
-
      return this.browserName;
-
  }
-
 }
