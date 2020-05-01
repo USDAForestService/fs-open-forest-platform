b802e7c51a8e7ca39eaaf0d2939aad58021e9622
brian davidson <brian.davidson.1981@gmail.com>
2020-04-30 20:54:56 -0400

    1182 - disabling test

--- frontend/src/app/trees/admin/season-dates/season-dates.component.spec.ts
+++ frontend/src/app/trees/admin/season-dates/season-dates.component.spec.ts
@@ -72,120 +72,120 @@ describe('Season Dates Admin Component', () => {
     }
   }
 
-  // describe('', () => {
-  //   beforeEach(
-  //     async(() => {
-  //       TestBed.configureTestingModule({
-  //         declarations: [AdminSeasonDatesComponent],
-  //         providers: [
-  //           ApplicationFieldsService,
-  //           {provide: ChristmasTreesApplicationService, useClass: MockApplicationService},
-  //           FormBuilder,
-  //           RouterTestingModule,
-  //           Title,
-  //           ChristmasTreesAdminService,
-  //           UtilService
-  //         ],
-  //         imports: [RouterTestingModule, HttpClientTestingModule],
-  //         schemas: [NO_ERRORS_SCHEMA]
-  //       }).compileComponents();
-  //     })
-  //   );
-  //
-  //   beforeEach(() => {
-  //     TestBed.overrideProvider(ActivatedRoute, {useValue: mockActivatedRoute});
-  //     fixture = TestBed.createComponent(AdminSeasonDatesComponent);
-  //     component = fixture.debugElement.componentInstance;
-  //     formBuilder = new FormBuilder();
-  //     component.form = formBuilder.group({
-  //       forestId: ['', [Validators.required]],
-  //       dateTimeRange: formBuilder.group({
-  //         endDateTime: [''],
-  //         endDay: [''],
-  //         endMonth: [''],
-  //         endYear: [''],
-  //         endHour: [''],
-  //         endMinutes: ['00'],
-  //         endPeriod: [''],
-  //         startDateTime: [''],
-  //         startDay: [''],
-  //         startMonth: [''],
-  //         startYear: [''],
-  //         startHour: [''],
-  //         startMinutes: ['00'],
-  //         startPeriod: ['']
-  //       })
-  //     });
-  //     fixture.detectChanges();
-  //   });
-  //
-  //   it('should create', () => {
-  //     expect(component).toBeTruthy();
-  //   });
-  //
-  //
-  //   it('should update season dates', () => {
-  //     component.updateStatus = '';
-  //     component.forest = {
-  //       id: 1,
-  //       forestName: 'Arapaho and Roosevelt National Forests',
-  //       description: 'Arapaho & Roosevelt | Colorado | Fort Collins, CO',
-  //       forestAbbr: 'arp'
-  //     };
-  //
-  //     component.dateStatus.hasErrors = false;
-  //     component.form.get('forestId').setValue('1');
-  //     component.form.get('dateTimeRange.startMonth').setValue('10');
-  //     component.form.get('dateTimeRange.startDay').setValue('10');
-  //     component.form.get('dateTimeRange.startYear').setValue('2017');
-  //     component.form.get('dateTimeRange.endMonth').setValue('10');
-  //     component.form.get('dateTimeRange.endDay').setValue('10');
-  //     component.form.get('dateTimeRange.endYear').setValue('2018');
-  //     expect(component.form.valid).toBeTruthy();
-  //     component.updateSeasonDates();
-  //   });
-  //
-  //   it('should update date status', () => {
-  //     component.updateDateStatus({
-  //       startDateTimeValid: false,
-  //       endDateTimeValid: false,
-  //       startBeforeEnd: false,
-  //       startAfterToday: false,
-  //       hasErrors: false,
-  //       dateTimeSpan: 0
-  //     });
-  //     expect(component.dateStatus).toEqual({
-  //       startDateTimeValid: false,
-  //       endDateTimeValid: false,
-  //       startBeforeEnd: false,
-  //       startAfterToday: false,
-  //       hasErrors: false,
-  //       dateTimeSpan: 0
-  //     });
-  //   });
-  //
-  //   it('should set start and end dates', () => {
-  //     component.forest = component.forests.find(forest => forest.id === 2);
-  //
-  //     component.setStartEndDate(component.forest, component.form);
-  //     expect(component.form.get('dateTimeRange.startMonth').value).toEqual('10');
-  //     expect(component.form.get('dateTimeRange.startDay').value).toEqual('31');
-  //     expect(component.form.get('dateTimeRange.startYear').value).toEqual('2018');
-  //     expect(component.form.get('dateTimeRange.endMonth').value).toEqual('09');
-  //     expect(component.form.get('dateTimeRange.endDay').value).toEqual('30');
-  //     expect(component.form.get('dateTimeRange.endYear').value).toEqual('2019');
-  //
-  //     component.forest = component.forests.find(forest => forest.id === 5);
-  //     component.setStartEndDate(component.forest, component.form);
-  //     expect(component.form.get('dateTimeRange.endYear').value).toEqual('2019');
-  //   });
-  //
-  //   it('should change the forest when the forestId changes', async(() => {
-  //     component.form.get('forestId').setValue(2);
-  //     fixture.whenStable().then(() => {
-  //       expect(component.form.get('dateTimeRange.startMonth').value).toEqual('10');
-  //     });
-  //   }));
-  // });
+  xdescribe('', () => {
+    beforeEach(
+      async(() => {
+        TestBed.configureTestingModule({
+          declarations: [AdminSeasonDatesComponent],
+          providers: [
+            ApplicationFieldsService,
+            {provide: ChristmasTreesApplicationService, useClass: MockApplicationService},
+            FormBuilder,
+            RouterTestingModule,
+            Title,
+            ChristmasTreesAdminService,
+            UtilService
+          ],
+          imports: [RouterTestingModule, HttpClientTestingModule],
+          schemas: [NO_ERRORS_SCHEMA]
+        }).compileComponents();
+      })
+    );
+
+    beforeEach(() => {
+      TestBed.overrideProvider(ActivatedRoute, {useValue: mockActivatedRoute});
+      fixture = TestBed.createComponent(AdminSeasonDatesComponent);
+      component = fixture.debugElement.componentInstance;
+      formBuilder = new FormBuilder();
+      component.form = formBuilder.group({
+        forestId: ['', [Validators.required]],
+        dateTimeRange: formBuilder.group({
+          endDateTime: [''],
+          endDay: [''],
+          endMonth: [''],
+          endYear: [''],
+          endHour: [''],
+          endMinutes: ['00'],
+          endPeriod: [''],
+          startDateTime: [''],
+          startDay: [''],
+          startMonth: [''],
+          startYear: [''],
+          startHour: [''],
+          startMinutes: ['00'],
+          startPeriod: ['']
+        })
+      });
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+
+    it('should update season dates', () => {
+      component.updateStatus = '';
+      component.forest = {
+        id: 1,
+        forestName: 'Arapaho and Roosevelt National Forests',
+        description: 'Arapaho & Roosevelt | Colorado | Fort Collins, CO',
+        forestAbbr: 'arp'
+      };
+
+      component.dateStatus.hasErrors = false;
+      component.form.get('forestId').setValue('1');
+      component.form.get('dateTimeRange.startMonth').setValue('10');
+      component.form.get('dateTimeRange.startDay').setValue('10');
+      component.form.get('dateTimeRange.startYear').setValue('2017');
+      component.form.get('dateTimeRange.endMonth').setValue('10');
+      component.form.get('dateTimeRange.endDay').setValue('10');
+      component.form.get('dateTimeRange.endYear').setValue('2018');
+      expect(component.form.valid).toBeTruthy();
+      component.updateSeasonDates();
+    });
+
+    it('should update date status', () => {
+      component.updateDateStatus({
+        startDateTimeValid: false,
+        endDateTimeValid: false,
+        startBeforeEnd: false,
+        startAfterToday: false,
+        hasErrors: false,
+        dateTimeSpan: 0
+      });
+      expect(component.dateStatus).toEqual({
+        startDateTimeValid: false,
+        endDateTimeValid: false,
+        startBeforeEnd: false,
+        startAfterToday: false,
+        hasErrors: false,
+        dateTimeSpan: 0
+      });
+    });
+
+    it('should set start and end dates', () => {
+      component.forest = component.forests.find(forest => forest.id === 2);
+
+      component.setStartEndDate(component.forest, component.form);
+      expect(component.form.get('dateTimeRange.startMonth').value).toEqual('10');
+      expect(component.form.get('dateTimeRange.startDay').value).toEqual('31');
+      expect(component.form.get('dateTimeRange.startYear').value).toEqual('2018');
+      expect(component.form.get('dateTimeRange.endMonth').value).toEqual('09');
+      expect(component.form.get('dateTimeRange.endDay').value).toEqual('30');
+      expect(component.form.get('dateTimeRange.endYear').value).toEqual('2019');
+
+      component.forest = component.forests.find(forest => forest.id === 5);
+      component.setStartEndDate(component.forest, component.form);
+      expect(component.form.get('dateTimeRange.endYear').value).toEqual('2019');
+    });
+
+    it('should change the forest when the forestId changes', async(() => {
+      component.form.get('forestId').setValue(2);
+      fixture.whenStable().then(() => {
+        expect(component.form.get('dateTimeRange.startMonth').value).toEqual('10');
+      });
+    }));
+  });
 
 });
