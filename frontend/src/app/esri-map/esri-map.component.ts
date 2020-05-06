7c6f9111f98a1a266fd6152d697362f8207e83fc
Michael Laney <mtlaney@gmail.com>
2020-05-06 17:40:59 -0400

    add wsm arcgis forest layer to esri map

--- frontend/src/app/esri-map/esri-map.component.ts
+++ frontend/src/app/esri-map/esri-map.component.ts
@@ -16,19 +16,27 @@ export class EsriMapComponent implements OnInit {
   ngOnInit() {
     loadModules([
       'esri/Map',
-      'esri/views/MapView'
+      'esri/views/MapView',
+      'esri/layers/WMSLayer'
     ])
-      .then(([EsriMap, EsriMapView]) => {
+      .then(([EsriMap, MapView, WMSLayer]) => {
         const map = new EsriMap({
-          basemap: 'topo-vector'
+          basemap: 'topo',
         });
 
-        const mapView = new EsriMapView({
+        const mapView = new MapView({
           container: this.mapViewEl.nativeElement,
           center: [-98, 38],
           zoom: 5,
           map: map
         });
+
+      const wmsForest = new WMSLayer({
+        url: 'https://apps.fs.usda.gov/arcx/services/EDW/EDW_ForestCommonNames_01/MapServer/WMSServer?request=GetCapabilities&service=WMS',
+      });
+
+      map.add(wmsForest);
+
       })
       .catch(err => {
         console.error(err);
