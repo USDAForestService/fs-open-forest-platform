dc5800e2f533fdf90c13743725d8765648377819
kilara77 <kilara77@gmail.com>
2020-05-06 14:13:40 -0400

    Update run-e2e.sh

--- .circleci/run-e2e.sh
+++ .circleci/run-e2e.sh
@@ -14,14 +14,3 @@ serverid=$!
 sleep 1
 cd ../frontend;
 npm run e2e $PARAMS;
-
-e2ereturncode=$?
-
-if [ $e2ereturncode = 0 ]
-then
-  echo 'SUCCESS'
-else
-  echo 'FAIL'
-fi
-kill -int $serverid
-exit $e2ereturncode
