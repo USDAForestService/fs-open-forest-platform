46ac6c8afe0564fae4ef498b586360ff71c5c243
kilara77 <kilara77@gmail.com>
2020-05-08 16:01:04 -0400

    Update karma.conf.js

--- frontend/development-configurations/karma.conf.js
+++ frontend/development-configurations/karma.conf.js
@@ -13,7 +13,10 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
+    client: {    
+      jasmine: {
+        random: false
+      },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
