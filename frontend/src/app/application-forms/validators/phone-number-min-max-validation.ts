e65e67b7316d3ea66a89fe7a571da4a5950bbf37
Dylan McAfee <dylan.mcafee@usda.gov>
2020-05-13 14:37:37 -0500

    Add min-max validator

---
+++ frontend/src/app/application-forms/validators/phone-number-min-max-validation.ts
@@ -0,0 +1,24 @@
+import { FormControl, ValidatorFn } from '@angular/forms';
+
+export function phoneNumberMinMaxValidator(): ValidatorFn {
+  return (control: FormControl): { [key: string]: any } => {
+    const val = control.value;
+    if (val && val.length) {
+      const phoneNumberRegex = /^[0-9]*$/;
+      const valid = phoneNumberRegex.test(val);
+      if (!valid) {
+        return {numberRequirement: true};
+      }
+      else if (valid && val.length < 10) {
+        return {phoneNumberMinRequirement: true};
+      }
+      else if (valid && val.length > 10) {
+        return {phoneNumberMaxRequirement: true};
+      }
+      else{
+        return null;
+      }
+    }
+  };
+}
+
