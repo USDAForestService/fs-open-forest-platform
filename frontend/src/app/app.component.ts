51e4d0feebedbd59737400abf23540dda0c89c00
Dmac26 <dylanmc21@gmail.com>
2020-06-12 10:25:50 -0500

    Changes for code style consistency

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -98,19 +98,19 @@ export class AppComponent implements OnInit {
     let browserInfo = userAgent.match (/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
     let parsedBrowserInfo;
 
-        if (/trident/i.test(browserInfo[1])) {
-          parsedBrowserInfo = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
-          return { name: 'IE', version: (parsedBrowserInfo[1] || '') };
-        }
-        if (browserInfo[1] === 'Chrome') {
-        parsedBrowserInfo = userAgent.match (/\bOPR|Edge\/(\d+)/);
-        if (parsedBrowserInfo != null)   {
-            return { name: 'Opera', version: parsedBrowserInfo[1] };
-          }
-        }
-      browserInfo = browserInfo[2] ? [browserInfo[1], browserInfo[2]] : [navigator.appName, navigator.appVersion, '-?'];
-      if (( parsedBrowserInfo = userAgent.match(/version\/(\d+)/i)) != null) {
-        browserInfo.splice(1, 1, parsedBrowserInfo[1]);
+    if (/trident/i.test(browserInfo[1])) {
+      parsedBrowserInfo = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
+      return { name: 'IE', version: (parsedBrowserInfo[1] || '') };
+    }
+    if (browserInfo[1] === 'Chrome') {
+      parsedBrowserInfo = userAgent.match (/\bOPR|Edge\/(\d+)/);
+      if (parsedBrowserInfo != null)   {
+        return { name: 'Opera', version: parsedBrowserInfo[1] };
+      }
+    }
+    browserInfo = browserInfo[2] ? [browserInfo[1], browserInfo[2]] : [navigator.appName, navigator.appVersion, '-?'];
+    if (( parsedBrowserInfo = userAgent.match(/version\/(\d+)/i)) != null) {
+      browserInfo.splice(1, 1, parsedBrowserInfo[1]);
     }
      this.browserName = browserInfo[0];
      return this.browserName;
