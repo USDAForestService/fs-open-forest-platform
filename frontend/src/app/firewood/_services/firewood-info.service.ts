081968b65e3f0c7822d7ad19ff9f68f93e42b627
brian davidson <brian.davidson.1981@gmail.com>
2020-06-23 21:32:24 -0400

    removed unused imports

--- frontend/src/app/firewood/_services/firewood-info.service.ts
+++ frontend/src/app/firewood/_services/firewood-info.service.ts
@@ -1,17 +1,12 @@
 /* tslint:disable:no-shadowed-variable prefer-const */
 
-
-import {mergeMap, map} from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable ,  forkJoin } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import * as moment from 'moment/moment';
 
 @Injectable()
 export class FirewoodInfoService {
   private endpoint = environment.apiUrl + 'forests/';
-  private CUTTING_AREA_KEYS = ['ELKCREEK', 'REDFEATHERLAKES', 'SULPHUR', 'CANYONLAKES'];
 
   constructor(private http: HttpClient) {}
 
