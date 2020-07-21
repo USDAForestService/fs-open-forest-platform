7765ab986582c71be64df0356c7460f318a36c90
Michael Laney <mtlaney@gmail.com>
2020-07-21 12:29:39 -0400

    add new api endpoint

--- server/dba/seeders/forests.js
+++ server/dba/seeders/forests.js
@@ -20,6 +20,28 @@ const forests = [
     state: 'Idaho',
     permit_type: 'fuel',
     region: 6
+  },
+  {
+    id: 2,
+    forest_name: 'Flathead National Forest',
+    forest_url: 'https://www.fs.usda.gov/flathead/',
+    start_date: '2020-04-01T08:00:00Z',
+    end_date: '2021-03-31T08:59:59Z',
+    created: 'now()',
+    updated: 'now()',
+    org_structure_code: '',
+    description: 'Flathead | Montana',
+    forest_abbr: 'flathead',
+    wood_cost: 5,
+    timezone: 'America/Boise',
+    cutting_areas: '{}',
+    map_links: '{}',
+    contact:
+    '{ "Flathead Supervisors Office / Tally Lake Ranger District": {"address": "650 Wolfpack Way", "citystate": "Kalispell, Montana", "phone": "(406) 758-5204"},',
+    poss_financial_id: '',
+    state: 'Montana',
+    permit_type: 'fuel',
+    region: 6
   }
 ];
 
