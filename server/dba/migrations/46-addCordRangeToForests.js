f4911e9d990b39056d46929335b4848ddb0fa959
Dmac26 <dylanmc21@gmail.com>
2020-08-31 14:39:41 -0500

    update seeders & add migration for min & max cords

---
+++ server/dba/migrations/46-addCordRangeToForests.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let doTransaction = require('./modules/transaction');
+
+const TABLE_NAME = 'fsForests';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    let operations = [
+      {
+        operation: 'add',
+        field: 'min_cords',
+        type: Sequelize.INTEGER
+      },
+      {
+        operation: 'add',
+        field: 'max_cords',
+        type: Sequelize.INTEGER
+      },
+    ];
+
+    return doTransaction(TABLE_NAME, queryInterface, operations);
+  },
+  down: function(queryInterface, Sequelize) {
+    let operations = [
+      {
+        operation: 'remove',
+        field: 'min_cords'
+      },
+      {
+        operation: 'remove',
+        field: 'max_cords'
+      }
+    ];
+
+    return doTransaction(TABLE_NAME, queryInterface, operations);
+  }
+};
