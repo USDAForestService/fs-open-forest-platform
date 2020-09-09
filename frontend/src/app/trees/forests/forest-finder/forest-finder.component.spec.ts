aa1b3929598e33ff5ffd02e8cc98fb137d372eb7
Michael Laney <mtlaney@gmail.com>
2020-09-09 14:38:29 -0400

    update tests

--- frontend/src/app/trees/forests/forest-finder/forest-finder.component.spec.ts
+++ frontend/src/app/trees/forests/forest-finder/forest-finder.component.spec.ts
@@ -1,69 +1,29 @@
-import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ForestFinderComponent } from './forest-finder.component';
-import { RouterTestingModule } from '@angular/router/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ChristmasTreesInfoService } from '../../_services/christmas-trees-info.service';
-import { RemovePuncPipe } from './remove-punc.pipe';
-import { Observable } from 'rxjs/Observable';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Location } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { SpacesToDashesPipe } from '../../../_pipes/spaces-to-dashes.pipe';
-import { UtilService } from '../../../_services/util.service';
 import { HttpClientModule } from '@angular/common/http';
 
 describe('ForestFinderComponent', () => {
   let component: ForestFinderComponent;
   let fixture: ComponentFixture<ForestFinderComponent>;
-  let router: Router;
-  let location: Location;
 
-  const mockActivatedRoute = {
-    params: Observable.of({ id: 1 }),
-    data: Observable.of({
-      forests: [
-        {
-          id: 1,
-          forestName: 'Arapaho and Roosevelt',
-          description: 'Arapaho & Roosevelt | Colorado | Fort Collins, CO',
-          forestAbbr: 'arp'
-        },
-        { id: 2, forestName: 'Flathead', description: 'Flathead | Montana | Kalispell, MT', forestAbbr: 'flathead' },
-        { id: 3, forestName: 'Mt. Hood', description: 'Mt. Hood | Oregon | Portland, OR', forestAbbr: 'mthood' },
-        {
-          id: 4,
-          forestName: 'Shoshone',
-          description: 'Shoshone | Montana, Wyoming | Cody, WY, Jackson, WY',
-          forestAbbr: 'shoshone'
-        }
-      ]
-    })
-  };
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [ForestFinderComponent, RemovePuncPipe, SpacesToDashesPipe],
+        declarations: [ForestFinderComponent],
         schemas: [NO_ERRORS_SCHEMA],
-        providers: [UtilService, { provide: ChristmasTreesInfoService, useClass: ChristmasTreesInfoService }],
+        providers: [],
         imports: [
           HttpClientModule,
           HttpClientTestingModule,
-          RouterTestingModule.withRoutes([
-            {
-              path: 'christmas-trees/forests/:id',
-              component: ForestFinderComponent
-            }
-          ])
         ]
       }).compileComponents();
     })
   );
 
   beforeEach(() => {
-    TestBed.overrideProvider(ActivatedRoute, { useValue: mockActivatedRoute });
-    router = TestBed.get(Router);
-    location = TestBed.get(Location);
     fixture = TestBed.createComponent(ForestFinderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -73,12 +33,8 @@ describe('ForestFinderComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it(
-    'should redirect to forest page on click',
-    fakeAsync(() => {
-      component.goToForest({ forestAbbr: 'arp' });
-      tick();
-      expect(location.path()).toBe('/christmas-trees/forests/arp');
-    })
-  );
+  it('should show trees has moved', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Christmas Trees has moved')
+  })
 });
