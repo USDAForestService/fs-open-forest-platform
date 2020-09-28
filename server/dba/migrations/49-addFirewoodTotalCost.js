4b8b2d44f853ac528c8470dc2eb021899e89be8d
Dmac26 <dylanmc21@gmail.com>
2020-09-28 11:09:40 -0500

    Add minCost

---
+++ server/dba/migrations/49-addFirewoodTotalCost.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let doTransaction = require('./modules/transaction');
+
+const TABLE_NAME = 'fsForests';
+
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        let operations = [
+        {
+            operation: 'add',
+            field: 'min_cost',
+            type: Sequelize.NUMERIC(10, 2)
+        },
+        ];
+
+        return doTransaction(TABLE_NAME, queryInterface, operations);
+    },
+    down: function(queryInterface, Sequelize) {
+        let operations = [
+        {
+            operation: 'remove',
+            field: 'min_cost'
+        }
+        ];
+
+        return doTransaction(TABLE_NAME, queryInterface, operations);
+    }
+};
