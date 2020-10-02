6a15705ca89b7139939f875c72f849f14d8c3805
Michael Laney <mtlaney@gmail.com>
2020-10-02 00:14:41 -0400

    add migration and seeder

--- server/dba/migrations/45-nrmService.js
+++ server/dba/migrations/45-nrmService.js
@@ -3,10 +3,29 @@
 module.exports = {
   up: function(queryInterface, Sequelize) {
     return queryInterface.createTable('nrmEntries', {
-      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true, field: 'id' },
-      message: { type: Sequelize.STRING, field: 'message' },
-      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW, allowNull: false, field: 'created' },
-      updatedAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW, allowNull: false, field: 'updated' }
+      permitCn: { type: Sequelize.STRING(50), field: 'permit_cn' },
+      regionName: { type: Sequelize.STRING(50), field: 'region_name' },
+      regionCode: { type: Sequelize.STRING(50), field: 'region_code' },
+      forestName: { type: Sequelize.STRING(50), field: 'forest_name' },
+      forestCode: { type: Sequelize.STRING(50), field: 'forest_code' },
+      districtCode: { type: Sequelize.STRING(50), field: 'district_code' },
+      districtName: { type: Sequelize.STRING(50), field: 'district_name' },
+      planCn: { type: Sequelize.STRING(50), field: 'plan_cn' },
+      planNo: { type: Sequelize.INTEGER, field: 'plan_no' },
+      planDescription: { type: Sequelize.STRING(50), field: 'plan_description' },
+      issueNumber: { type: Sequelize.STRING(50), field: 'issue_number' },
+      issueDate: { type: Sequelize.DATE, field: 'issue_date' },
+      permUseCode: { type: Sequelize.STRING(50), field: 'perm_use_code' },
+      percentOfSalvageVolume: { type: Sequelize.INTEGER, field: 'percent_of_salvage_volume' },
+      percentOfCwk2Volume: { type: Sequelize.INTEGER, field: 'percent_of_cwk2_volume' },
+      percentOfCflrVolume: { type: Sequelize.INTEGER, field: 'percent_of_cflr_volume' },
+      percentOfNftmVolume: { type: Sequelize.INTEGER, field: 'percent_of_nftm_volume' },
+      stateCode: { type: Sequelize.STRING(50), field: 'state_code' },
+      stateName: { type: Sequelize.STRING(50), field: 'state_name' },
+      numberOfPermits: { type: Sequelize.INTEGER, field: 'number_of_permits' },
+      convertibleNonConvertible: { type: Sequelize.STRING(50), field: 'convertible_non_convertible' },
+      spuInfo: { type: Sequelize.JSONB, field: 'spu_info' },
+
     });
   },
   down: function(queryInterface, Sequelize) {
--- server/dba/seeders/02-nrmData.js
+++ server/dba/seeders/02-nrmData.js
@@ -1,9 +1,38 @@
 const data = [
   {
-    id: 1,
-    message: 'successful connection test',
-    created: 'now()',
-    updated: 'now()'
+    permit_cn: 'OF48303820',
+    region_code: '01',
+    region_name: 'Northern',
+    forest_code: '10',
+    forest_name: 'Flathead National Forest',
+    district_code: '08',
+    district_name: 'Tally Lake',
+    plan_cn: 'OF011008',
+    plan_no: 100011008,
+    plan_description: 'OF Firewood Permits',
+    issue_number: 'OF01',
+    issue_date: '2020-11-18T08:00:00.000Z',
+    perm_use_code: '2',
+    percent_of_salvage_volume: 100,
+    percent_of_cwk2_volume: 0,
+    percent_of_cflr_volume: 0,
+    percent_of_nftm_volume: 0,
+    state_code: '30',
+    state_name: 'Montana',
+    number_of_permits: 1,
+    convertible_non_convertible: 'C',
+    spu_info:
+      '{'
+       + '"line_item_number": "1",'
+       + '"species_code": "DOUGLASFIR",'
+       + '"product_code": "07",'
+       + '"uom_code": "02",'
+       + '"sold_quantity": "1",'
+       + '"rate": "5.00",'
+       + '"yield_component_code": "CD",'
+       + '"mbf_conv_factor": "2",'
+       + '"ccf_con_factor": "2"'
+      + '}'
   }
 ];
 module.exports = {
